958bc326d870bf7f51549655f34a877f2f6f27c7
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-17 00:35:56 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Wait for the HTML document to be fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function () {
+    // Your code goes here
+
+    // Example: Display an alert when the page is loaded
+    alert('Welcome to Your Website!');
+});
