ef9ad67b8e8ce104297d9cafa2211faf7aad0c4b
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-17 20:35:41 -0500

    update
    update

--- main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-
-// Wait for the HTML document to be fully loaded before running the script
-document.addEventListener('DOMContentLoaded', function () {
-    // Your code goes here
-
-    // Example: Display an alert when the page is loaded
-    alert('Welcome to Your Website!');
-});
