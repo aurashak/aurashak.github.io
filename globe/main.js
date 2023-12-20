e02e8764796513a308a55032b3b16ebdfa2f5e52
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-19 23:49:35 -0500

    Update main.js

--- globe/main.js
+++ globe/main.js
@@ -1,25 +1,49 @@
+// main.js
+
+// Set up scene, camera, and renderer
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Use SphereGeometry instead of BoxGeometry
-const geometry = new THREE.SphereGeometry(2, 32, 32); // Adjusted sphere size and segments
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const sphere = new THREE.Mesh(geometry, material); // Changed variable name to 'sphere'
-scene.add(sphere); // Changed from 'cube' to 'sphere'
+// Create a plane (representing the map)
+const geometry = new THREE.PlaneGeometry(10, 5); // Adjust the width and height as needed
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const map = new THREE.Mesh(geometry, material);
+scene.add(map);
 
+// Load GeoJSON data for country outlines
+const loader = new THREE.GeoJsonLoader();
+loader.load('path/to/countries.geojson', function (geojson) {
+  const countries = new THREE.Mesh(
+    new THREE.Geometry().fromBufferGeometry(new THREE.BufferGeometry().fromGeometry(geojson)),
+    new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+  );
+  scene.add(countries);
+});
+
+// Position the camera
+camera.position.z = 10;
+
+// Add lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
+
+// Animation function
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.005;
-  sphere.rotation.y += 0.01;
+  // Rotate the map slowly
+  map.rotation.y += 0.005;
 
+  // Render the scene
   renderer.render(scene, camera);
 }
 
+// Start animation
 animate();
