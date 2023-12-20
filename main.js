17edf3901e505138cb85193e8560bd83d26c03b3
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-20 00:34:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-// main.js
-
-// Set up scene, camera, and renderer
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a colored cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Green color
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Use SphereGeometry instead of BoxGeometry
+const geometry = new THREE.SphereGeometry(2, 32, 32); // Adjusted sphere size and segments
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const sphere = new THREE.Mesh(geometry, material); // Changed variable name to 'sphere'
+scene.add(sphere); // Changed from 'cube' to 'sphere'
 
-// Position the camera
-camera.position.z = 5;
-
-// Animation function
 function animate() {
   requestAnimationFrame(animate);
 
-  // Rotate the cube slowly
-  cube.rotation.x += 0.005;
-  cube.rotation.y += 0.005;
+  sphere.rotation.x += 0.005;
+  sphere.rotation.y += 0.01;
 
-  // Render the scene
   renderer.render(scene, camera);
 }
 
-// Start animation
 animate();
