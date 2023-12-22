6731232102ccbe1ef607a467b366f65c6291ca2c
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-21 22:56:02 -0500

    Add files via upload

--- dev/common.js
+++ dev/common.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Get the base URL of the current page
+    const baseURL = window.location.href.replace(/\/[^/]+$/, '/');
+
     // Load common header
-    fetch("header.html")
+    fetch(baseURL + "header.html")
       .then(response => response.text())
       .then(html => {
         document.getElementById("header-placeholder").innerHTML = html;
       });
   
     // Load common navigation
-    fetch("navigation.html")
+    fetch(baseURL + "navigation.html")
       .then(response => response.text())
       .then(html => {
         document.getElementById("navigation-placeholder").innerHTML = html;
       });
   
     // Load common footer
-    fetch("footer.html")
+    fetch(baseURL + "footer.html")
       .then(response => response.text())
       .then(html => {
         document.getElementById("footer-placeholder").innerHTML = html;
