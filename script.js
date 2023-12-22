30e4014da8adb3e748c5471135363a04fdcdad9d
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 16:38:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
-// Create a scene
-const scene = new THREE.Scene();
-
-// Create a camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a WebGLRenderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('globe-container').appendChild(renderer.domElement);
-
-// Create a sphere
-const geometry = new THREE.SphereGeometry(2, 32, 32);
-const material = new THREE.LineBasicMaterial({ color: 0x000000 });
-const sphere = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), material);
-scene.add(sphere);
-
-// Load your country outline data and create line segments for each country
-// Example: const countryOutlines = createCountryOutlines();
-// Add country outlines as line segments to the scene
-
-// Handle window resize
-window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-
-    camera.aspect = newWidth / newHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(newWidth, newHeight);
-});
-
-// Animation loop
-const animate = () => {
-    requestAnimationFrame(animate);
-
-    // Add rotation logic here
-    sphere.rotation.y += 0.005;
-
-    renderer.render(scene, camera);
-};
-
-animate();
+// Ensure that the DOM is fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', function () {
+    // Set up the scene, camera, and renderer
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('cube-container').appendChild(renderer.domElement);
+  
+    // Create a cube
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+  
+    // Position the camera
+    camera.position.z = 5;
+  
+    // Define an animation function
+    var animate = function () {
+      requestAnimationFrame(animate);
+  
+      // Rotate the cube
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+  
+      // Render the scene
+      renderer.render(scene, camera);
+    };
+  
+    // Start the animation
+    animate();
+  });
+  
