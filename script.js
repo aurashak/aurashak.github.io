f7af25ea702bce89e39549469228236ee23aaa40
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 16:41:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Ensure that the DOM is fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function () {
     // Set up the scene, camera, and renderer
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+  
+    // Corrected ID here: 'cube-container'
     document.getElementById('cube-container').appendChild(renderer.domElement);
   
     // Create a cube
