b99e555a0882355ead22be953cf61df2c1873ebd
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 23:28:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,79 +6,95 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('cube-container').appendChild(renderer.domElement);
 
-    var initialSphereSize = 2 * 1.1; // Increase the initial size by 10%
+    var initialSphereSize = 2;
     var geometry = new THREE.SphereGeometry(initialSphereSize, 32, 32);
     var material = new THREE.MeshPhongMaterial({ color: 0x888888 });
     var sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
-  
+
     var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(5, 5, 5);
     scene.add(directionalLight);
-  
+
     var graticule = new THREE.Object3D();
     var graticuleSpacing = 0.1;
-  
-    for (let i = -1; i <= 1; i += graticuleSpacing) {
-      var verticalGeometry = new THREE.BufferGeometry();
-      var verticalMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
-      verticalGeometry.setAttribute('position', new THREE.Float32BufferAttribute([i, -1, 0, i, 1, 0], 3));
-      var verticalLine = new THREE.Line(verticalGeometry, verticalMaterial);
-      graticule.add(verticalLine);
-  
-      var horizontalGeometry = new THREE.BufferGeometry();
-      var horizontalMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
-      horizontalGeometry.setAttribute('position', new THREE.Float32BufferAttribute([-1, i, 0, 1, i, 0], 3));
-      var horizontalLine = new THREE.Line(horizontalGeometry, horizontalMaterial);
-      graticule.add(horizontalLine);
+
+    for (let lat = -90; lat <= 90; lat += graticuleSpacing) {
+        var latLineGeometry = new THREE.BufferGeometry();
+        var latLineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+        latLineGeometry.setAttribute('position', new THREE.Float32BufferAttribute([
+            -1, Math.sin(THREE.Math.degToRad(lat)), Math.cos(THREE.Math.degToRad(lat)),
+            1, Math.sin(THREE.Math.degToRad(lat)), Math.cos(THREE.Math.degToRad(lat))
+        ], 3));
+        var latLine = new THREE.Line(latLineGeometry, latLineMaterial);
+        graticule.add(latLine);
     }
-  
+
+    for (let lon = -180; lon <= 180; lon += graticuleSpacing) {
+        var lonLineGeometry = new THREE.BufferGeometry();
+        var lonLineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+        lonLineGeometry.setAttribute('position', new THREE.Float32BufferAttribute([
+            Math.sin(THREE.Math.degToRad(lon)), -1, Math.cos(THREE.Math.degToRad(lon)),
+            Math.sin(THREE.Math.degToRad(lon)), 1, Math.cos(THREE.Math.degToRad(lon))
+        ], 3));
+        var lonLine = new THREE.Line(lonLineGeometry, lonLineMaterial);
+        graticule.add(lonLine);
+    }
+
     scene.add(graticule);
-  
+
     camera.position.z = 5;
-  
+
     var mouseDown = false;
     var mouseX = 0;
     var mouseY = 0;
-  
+
     document.addEventListener('mousedown', function (event) {
-      mouseDown = true;
-      mouseX = event.clientX;
-      mouseY = event.clientY;
-    });
-  
-    document.addEventListener('mouseup', function () {
-      mouseDown = false;
-    });
-  
-    document.addEventListener('mousemove', function (event) {
-      if (mouseDown) {
-        var deltaX = event.clientX - mouseX;
-        var deltaY = event.clientY - mouseY;
-  
-        sphere.rotation.y += deltaX * 0.01;
-        sphere.rotation.x += deltaY * 0.01;
-  
+        mouseDown = true;
         mouseX = event.clientX;
         mouseY = event.clientY;
-      }
     });
-  
+
+    document.addEventListener('mouseup', function () {
+        mouseDown = false;
+    });
+
+    document.addEventListener('mousemove', function (event) {
+        if (mouseDown) {
+            var deltaX = event.clientX - mouseX;
+            var deltaY = event.clientY - mouseY;
+
+            sphere.rotation.y += deltaX * 0.01;
+            sphere.rotation.x += deltaY * 0.01;
+
+            mouseX = event.clientX;
+            mouseY = event.clientY;
+        }
+    });
+
     window.addEventListener('resize', function () {
-      var newWidth = window.innerWidth;
-      var newHeight = window.innerHeight;
-  
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(newWidth, newHeight);
+        var newWidth = window.innerWidth;
+        var newHeight = window.innerHeight;
+
+        camera.aspect = newWidth / newHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(newWidth, newHeight);
     });
-  
+
+    // Regular slow rotation
+    var rotationSpeed = 0.005;
+
     var animate = function () {
-      requestAnimationFrame(animate);
-  
-      renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+
+        // Only rotate the sphere when not clicked and dragged
+        if (!mouseDown) {
+            sphere.rotation.x += rotationSpeed;
+            sphere.rotation.y += rotationSpeed;
+        }
+
+        renderer.render(scene, camera);
     };
-  
+
     animate();
-  });
-  
+});
