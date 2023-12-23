f074b7a17b4a69c6a8b1b95a06b9ef0ba1a9d5b4
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 22:49:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // Set up the scene, camera, and renderer
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer({ alpha: true }); // Set alpha to true for a transparent background
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
   
     // Append renderer's dom element to cube-container
     document.getElementById('cube-container').appendChild(renderer.domElement);
   
-    // Create a cube with a solid gray material
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshPhongMaterial({ color: 0x888888 }); // Phong material for better lighting
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-  
-    // Position the cube
-    cube.position.set(0, 0, 0);
+    // Create a sphere with a solid gray material
+    var initialSphereSize = 2; // Adjust the size as needed
+    var geometry = new THREE.SphereGeometry(initialSphereSize, 32, 32);
+    var material = new THREE.MeshPhongMaterial({ color: 0x888888 });
+    var sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
   
     // Add a directional light
     var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -29,9 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var animate = function () {
       requestAnimationFrame(animate);
   
-      // Rotate the cube
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      // Rotate the sphere
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
   
       // Render the scene
       renderer.render(scene, camera);
