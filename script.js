febf51b75b703980983be61913340d20fe084409
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 23:22:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-  
+
     document.getElementById('cube-container').appendChild(renderer.domElement);
-  
-    var initialSphereSize = 2;
+
+    var initialSphereSize = 2 * 1.1; // Increase the initial size by 10%
     var geometry = new THREE.SphereGeometry(initialSphereSize, 32, 32);
     var material = new THREE.MeshPhongMaterial({ color: 0x888888 });
     var sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
+
+    // Rest of your code remains unchanged...
+});
+
   
     var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(5, 5, 5);
