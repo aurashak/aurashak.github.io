b6445cb0e54455d2c0ae8fb2be45b377674ebdef
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-22 21:52:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Set up the scene, camera, and renderer
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer({ alpha: true }); // Set alpha to true for a transparent background
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
   
     // Append renderer's dom element to cube-container
     document.getElementById('cube-container').appendChild(renderer.domElement);
   
     // Create a sphere with a solid gray material
-    var geometry = new THREE.SphereGeometry(1, 32, 32); // Adjust the sphere size and segments
-    var material = new THREE.MeshPhongMaterial({ color: 0x888888 });
+    var geometry = new THREE.SphereGeometry(5, 32, 32); // Increase the size and segmentation
+    var material = new THREE.MeshPhongMaterial({ color: 0x888888, wireframe: false });
     var sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
-  
-    // Position the sphere
-    sphere.position.set(0, 0, 0);
   
     // Add a directional light
     var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -23,27 +20,44 @@ document.addEventListener('DOMContentLoaded', function () {
     scene.add(directionalLight);
   
     // Set up the camera position
-    camera.position.z = 5;
+    camera.position.z = 15;
+  
+    // Enable manual rotation with right-click
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableRotate = true;
+    controls.enableZoom = true;
+  
+    // Create graticule lines every 10 degrees
+    var graticuleMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+    var graticuleGeometry = new THREE.BufferGeometry();
+    var graticulePoints = [];
+    var graticuleRadius = 5;
+  
+    for (let i = 0; i <= 36; i++) {
+      var theta = (i / 36) * Math.PI * 2;
+      var x = graticuleRadius * Math.cos(theta);
+      var y = graticuleRadius * Math.sin(theta);
+  
+      graticulePoints.push(new THREE.Vector3(x, y, 0));
+    }
+  
+    graticuleGeometry.setFromPoints(graticulePoints);
+  
+    var graticule = new THREE.Line(graticuleGeometry, graticuleMaterial);
+    scene.add(graticule);
   
     // Define an animation function
     var animate = function () {
       requestAnimationFrame(animate);
   
       // Rotate the sphere
-      sphere.rotation.x += 0.01;
-      sphere.rotation.y += 0.01;
+      sphere.rotation.y += 0.005;
   
       // Render the scene
       renderer.render(scene, camera);
     };
   
-    // Handle window resize
-    window.addEventListener('resize', function () {
-      var newWidth = window.innerWidth;
-      var newHeight = window.innerHeight;
-  
-      // Update camera aspect ratio and renderer size
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-      rendere
+    // Start the animation
+    animate();
+  });
   
