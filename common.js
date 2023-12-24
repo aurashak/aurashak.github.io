f4b82ba97ecc35db5875d831537ce1414556b26d
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-24 18:11:45 -0500

    Add files via upload

---
+++ common.js
@@ -0,0 +1,17 @@
+function loadElement(elementId, filePath) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {document.getElementById(elementId).innerHTML = this.responseText;}
+            if (this.status === 404) {document.getElementById(elementId).innerHTML = "Page not found.";}
+        }
+    }
+    xhttp.open("GET", filePath, true);
+    xhttp.send();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadElement("header", "header.html");
+    loadElement("navigation", "navigation.html");
+    loadElement("footer", "footer.html");
+});
