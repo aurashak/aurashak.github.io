414e4c4ddc6461ef22aa46a0694feab9a78f18be
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-26 20:57:13 -0500

    Add files via upload

--- common.js
+++ common.js
@@ -1,19 +1,33 @@
-function loadElement(elementId, filePath) {
+function loadElement(elementId, filePath, callback) {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4) {
-            if (this.status === 200) { document.getElementById(elementId).innerHTML = this.responseText; }
-            if (this.status === 404) { document.getElementById(elementId).innerHTML = "Page not found."; }
+            if (this.status === 200) { 
+                document.getElementById(elementId).innerHTML = this.responseText;
+                if (callback) callback();
+            }
+            if (this.status === 404) { 
+                document.getElementById(elementId).innerHTML = "Page not found."; 
+            }
         }
     }
     xhttp.open("GET", filePath, true);
     xhttp.send();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    loadElement("navigation", "https://aurashak.github.io/navigation.html");
-    loadElement("worknav", "https://aurashak.github.io/worknav.html");
+function highlightActiveLink() {
+    var navLinks = document.querySelectorAll('.UnderlineNav-item');
+    navLinks.forEach(function(link) {
+        if (link.href === window.location.href) {
+            link.classList.add('active-link');
+        }
+    });
+}
 
-    loadElement("printnav", "https://aurashak.github.io/nav/printnav.html");
+document.addEventListener("DOMContentLoaded", function() {
+    loadElement("navigation", "https://aurashak.github.io/navigation.html", highlightActiveLink);
+    loadElement("worknav", "https://aurashak.github.io/worknav.html", highlightActiveLink);
+
+    // Load other elements if needed
     loadElement("footer", "https://aurashak.github.io/footer.html");
 });
