be3993e5e0fd0f676ba8db54a05ef7b2e351cd67
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-29 01:39:33 -0500

    Add files via upload

--- test/testm.js
+++ test/testm.js
@@ -1,14 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
-        view: new ol.View({
-            center: ol.proj.fromLonLat([0, 0]), // Change to your desired center
-            zoom: 2 // Change to your desired initial zoom level
-        })
-    });
-});
+var countryStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'black' // Color of countries
+    }),
+    stroke: new ol.style.Stroke({
+      color: 'black', // Color of country borders
+      width: 1
+    })
+  });
+  
+  var vectorLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: 'https://aurashak.github.io/geojson/countries.geojson',
+      format: new ol.format.GeoJSON()
+    }),
+    style: countryStyle
+  });
+  
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }),
+      vectorLayer
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([0, 0]),
+      zoom: 2
+    })
+  });
+  
+  // Set the map background to white which will style the oceans
+  map.on('postrender', function() {
+    map.getViewport().style.background = 'white';
+  });
+  
