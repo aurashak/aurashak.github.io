787f44b5d29511adc00695eb3b66cb63f930550d
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-28 23:48:38 -0500

    Add files via upload

--- test/testm.js
+++ test/testm.js
@@ -1,12 +1,14 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
+document.addEventListener('DOMContentLoaded', function() {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([0, 0]), // Change to your desired center
+            zoom: 2 // Change to your desired initial zoom level
         })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([-74.0060, 40.7128]), // Coordinates for New York City
-        zoom: 10
-    })
+    });
 });
