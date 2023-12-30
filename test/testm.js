147c85516791c56d6cabe06a80b3a4b9ec934630
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-30 00:26:09 -0500

    Add files via upload

--- test/testm.js
+++ test/testm.js
@@ -1,23 +1,66 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      // Stamen Terrain Background layer
-      new ol.layer.Tile({
-        source: new ol.source.XYZ({
-          url: 'https://stamen-tiles.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.png',
-          attributions: [
-            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
-            '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; ' +
-            'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-          ],
-          maxZoom: 18
-        })
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-74.0060, 40.7128]), // Coordinates for New York City
-      zoom: 11 // Adjusted zoom level
+var mymap = L.map('mapid').setView([0, 0], 3);
+
+// Define layers
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+});
+var satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '© Esri and others'
+});
+// Define Copernicus (Sentinel) satellite tile layer
+var satelliteLayer = L.tileLayer('https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/default/GoogleMapsCompatible/{z}/{y}/{x}.jpg', {
+    attribution: '© EOX IT Services GmbH - Source: contains modified Copernicus Sentinel data 2020'
+});
+var geojsonLayer;
+
+// Initially add only the GeoJSON layer
+fetch('https://aurashak.github.io/geojson/countries.geojson')
+    .then(function(response) {
+        return response.json();
     })
-  });
-  
+    .then(function(data) {
+        geojsonLayer = L.geoJSON(data, {
+            style: function(feature) {
+                return {
+                    color: 'white',
+                    weight: 2,
+                    fillColor: 'black',
+                    fillOpacity: 1
+                };
+            }
+        }).addTo(mymap);
+    })
+    .catch(function(error) {
+        console.log('Error: ' + error);
+    });
+
+// Toggle functions
+function toggleOSMLayer() {
+    if (mymap.hasLayer(osmLayer)) {
+        mymap.removeLayer(osmLayer);
+    } else {
+        mymap.addLayer(osmLayer);
+        mymap.removeLayer(satelliteLayer);
+        mymap.removeLayer(geojsonLayer);
+    }
+}
+
+function toggleSatelliteLayer() {
+    if (mymap.hasLayer(satelliteLayer)) {
+        mymap.removeLayer(satelliteLayer);
+    } else {
+        mymap.addLayer(satelliteLayer);
+        mymap.removeLayer(osmLayer);
+        mymap.removeLayer(geojsonLayer);
+    }
+}
+
+function toggleGeoJSONLayer() {
+    if (mymap.hasLayer(geojsonLayer)) {
+        mymap.removeLayer(geojsonLayer);
+    } else {
+        mymap.addLayer(geojsonLayer);
+        mymap.removeLayer(osmLayer);
+        mymap.removeLayer(satelliteLayer);
+    }
+}
