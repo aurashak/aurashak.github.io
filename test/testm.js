1ce2e2d0390165ce46103694497916cc0c28ef5c
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-31 11:45:40 -0500

    Add files via upload

--- test/testm.js
+++ test/testm.js
@@ -1,75 +1,75 @@
+// Initialize the map on the 'mapid' div with a given center and zoom level.
+// Set minimum and maximum zoom levels to control zoom behavior.
 var mymap = L.map('mapid', {
     minZoom: 2,
     maxZoom: 7,
 }).setView([0, 0], 3);
 
-// Define tile layers
+// Define the OpenStreetMap layer and add it to the map.
 var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
-});
+}).addTo(mymap);
 
+// Define a satellite imagery layer.
 var satelliteLayer = L.tileLayer('https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/default/GoogleMapsCompatible/{z}/{y}/{x}.jpg', {
     attribution: '© EOX IT Services GmbH - Source: contains modified Copernicus Sentinel data 2020'
 });
 
-// GeoJSON layer
-var geojsonLayer = L.geoJSON(null, {
-    style: function(feature) {
-        return {color: 'white', weight: 0.5, fillColor: 'black', fillOpacity: 1};
-    },
-    onEachFeature: function(feature, layer) {
-        layer.on('mouseover', function() {
-            layer.bindTooltip(feature.properties.NAME, {permanent: true, direction: 'center'}).openTooltip();
-        });
-        layer.on('mouseout', function() {
-            layer.unbindTooltip();
-        });
-    }
-}).addTo(mymap);
+// Create a layer group for GeoJSON data and add it to the map.
+var geojsonGroup = L.layerGroup().addTo(mymap);
 
-// Add GeoJSON data
-fetch('https://aurashak.github.io/geojson/countries.geojson')
-    .then(response => response.json())
-    .then(data => {
-        geojsonLayer.addData(data);
-    });
+// Function to load and add GeoJSON data to the geojsonGroup layer.
+function addGeoJSONToGroup(url, style) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            L.geoJSON(data, { style: style }).addTo(geojsonGroup);
+        });
+}
 
-// Graticule
+// Load and style GeoJSON data for countries, lakes, and rivers.
+addGeoJSONToGroup('https://aurashak.github.io/geojson/countries.geojson', {
+    color: 'white',
+    weight: 0.5,
+    fillColor: 'black',
+    fillOpacity: 1
+});
+
+// Add a graticule (a network of latitude and longitude lines) to the map.
 L.graticule({
     interval: 20
 }).addTo(mymap);
 
-// Toggle layer functions
+// Functions to toggle different layers on the map.
 function toggleLayer(layer, otherLayers) {
     if (mymap.hasLayer(layer)) {
         mymap.removeLayer(layer);
     } else {
         mymap.addLayer(layer);
-        otherLayers.forEach(function(l) {
-            mymap.removeLayer(l);
-        });
+        otherLayers.forEach(l => mymap.removeLayer(l));
     }
 }
 
 function toggleOSMLayer() {
-    toggleLayer(osmLayer, [satelliteLayer, geojsonLayer]);
+    toggleLayer(osmLayer, [satelliteLayer, geojsonGroup]);
 }
 
 function toggleSatelliteLayer() {
-    toggleLayer(satelliteLayer, [osmLayer, geojsonLayer]);
+    toggleLayer(satelliteLayer, [osmLayer, geojsonGroup]);
 }
 
 function toggleGeoJSONLayer() {
-    toggleLayer(geojsonLayer, [osmLayer, satelliteLayer]);
+    toggleLayer(geojsonGroup, [osmLayer, satelliteLayer]);
 }
 
-// Add the search control to the map
+// Add a geocoding search control to the map using Nominatim service.
 var searchControl = new L.Control.geocoder({
     placeholder: "Search for a place",
     geocoder: new L.Control.Geocoder.Nominatim()
 }).addTo(mymap);
 
-// Event listener for the search button
+// Event listener for the custom search button.
+// Performs geocoding and zooms to the result location.
 document.getElementById('search-button').addEventListener('click', function() {
     var query = document.getElementById('search-input').value;
     L.Control.Geocoder.nominatim().geocode(query, function(results) {
