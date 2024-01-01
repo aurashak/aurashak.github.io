ca456aa9042d2493abbac9ec0220158b6e18f034
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 16:26:05 -0500

    Add files via upload

--- cesiumt.js
+++ cesiumt.js
@@ -1,5 +1,5 @@
 // Your access token from Cesium ion
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMjAyN2RmMC05ZDQxLTQwM2YtOWZiZC1hMTI5ZDZlMDgyMGIiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDM4MzA3Njh9.5yn30zsnLQltPUj52_wu8sNHKKNeHkGVi267uKmzI3Q';
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
 // Create a new Cesium Viewer in the div with the ID 'cesiumContainer'
 const viewer = new Cesium.Viewer('cesiumContainer', {
