25257a054b3732f321ef7875996710efa5803f72
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 02:11:36 -0500

    Add files via upload

--- gmt.js
+++ gmt.js
@@ -1,27 +1,61 @@
 function initMap() {
+    var afghanistan = {lat: 34.5553, lng: 69.2075}; // Coordinates for a location in Afghanistan
+
+    // Initialize the map
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
-        center: {lat: 0, lng: 0},
+        center: afghanistan, 
+        minZoom: 2,
+        maxZoom: 7,
         disableDefaultUI: true,
-        backgroundColor: 'white',
-        styles: [
-            // Hide all default map features
-            {
-                featureType: 'all',
-                elementType: 'all',
-                stylers: [{ visibility: 'off' }]
-            }
-        ]
+        backgroundColor: 'white'
+        // Other map options...
     });
 
-    // Load your GeoJSON.
+    // Load the GeoJSON file
     map.data.loadGeoJson('https://aurashak.github.io/geojson/countries.geojson');
 
-    // Define a style for your GeoJSON features
+    // Style the GeoJSON features
     map.data.setStyle({
         strokeColor: 'white',
         strokeWeight: 0.5,
         fillColor: 'black',
         fillOpacity: 1
+    });
+
+    // Create the search box and link it to the UI element
+    var input = document.getElementById('map-search');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards the current map's viewport
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    // Listen for the event fired when the user selects a prediction
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length === 0) {
+            return;
+        }
+
+        // For each place, get the icon, name, and location
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
     });
 }
