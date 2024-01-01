09edbf36ec4571b5dedd5f825a96582dc3a1438a
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 01:01:39 -0500

    Add files via upload

--- gmt.js
+++ gmt.js
@@ -1,9 +1,25 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2, // Set a zoom level that shows a good portion of the globe
-        center: {lat: 0, lng: 0} // Center the map at 0 degrees latitude and longitude
+        zoom: 2,
+        center: {lat: 0, lng: 0},
+        disableDefaultUI: true, // Disables the default UI controls
+        styles: [
+            {
+                featureType: 'all',
+                elementType: 'all',
+                stylers: [{visibility: 'off'}] // Turn off visibility of all features
+            }
+        ]
     });
 
-    // Load a GeoJSON layer onto the map
+    // Then load your GeoJSON.
     map.data.loadGeoJson('https://aurashak.github.io/geojson/countries.geojson');
+
+    // Optional: Define a style for your GeoJSON features
+    map.data.setStyle({
+        strokeColor: 'blue',
+        strokeWeight: 1,
+        fillColor: 'blue',
+        fillOpacity: 0.1
+    });
 }
