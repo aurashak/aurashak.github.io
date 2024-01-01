65369d29768a5d760092a528adb0b56e43bdb6e9
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 18:42:34 -0500

    Add files via upload

--- cesiumt.js
+++ cesiumt.js
@@ -1,18 +1,10 @@
-
 // Your access token from Cesium ion
+
+window.CESIUM_BASE_URL = './';
+
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
-// Create a new Cesium Viewer in the div with the ID 'cesiumContainer'
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    terrainProvider: Cesium.createWorldTerrain(),
-    animation: true,
-    baseLayerPicker: true,
-    geocoder: true,
-    homeButton: true,
-    infoBox: true,
-    sceneModePicker: true,
-    selectionIndicator: true,
-    timeline: true,
-    navigationHelpButton: true,
-    fullscreenButton: true,
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider : new Cesium.IonImageryProvider({ assetId: 2 }),
+    terrainProvider : Cesium.createWorldTerrain()
 });
