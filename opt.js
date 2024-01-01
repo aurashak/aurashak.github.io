d4b538eb9ceab44f9e4da16fb0e9b98dd3468a3c
aurashak <154039021+aurashak@users.noreply.github.com>
2023-12-31 21:21:55 -0500

    Add files via upload

--- opt.js
+++ opt.js
@@ -1,13 +1,12 @@
 var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+  target: 'mapid',  // Make sure this matches the ID of your map container
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
     })
-  });
-  
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([37.41, 8.82]), // Longitude and latitude of the initial center of the map
+    zoom: 4  // Initial zoom level
+  })
+});
