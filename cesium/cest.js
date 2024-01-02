d043a240454f16552fa4bc5670986401bf912cdc
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 22:11:44 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -12,7 +12,7 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     // Automatically track the entity
     timeline: false,
     // Display the "home" button - set to false to hide
-    homeButton: true,
+    homeButton: false,
     // Display the navigation help button - set to false to hide
     navigationHelpButton: false,
     // Display the geocoder - set to false to hide
