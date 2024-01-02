941dbdef3040c425d6c52685daa451d95a523d0e
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 19:45:01 -0500

    Add files via upload

---
+++ cesium/cest.js
@@ -0,0 +1,5 @@
+// script.js file
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
+var viewer = new Cesium.Viewer('cesiumContainer');
+
+
