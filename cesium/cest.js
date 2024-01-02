80e0cf534d61af0c8bafee23a0b7ce32e040d30a
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-02 01:15:25 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -64,11 +64,19 @@ viewer.screenSpaceEventHandler.setInputAction(function onMouseClick(movement) {
 }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
 
-// Load a local GeoJSON file (make sure the file is accessible from your server)
+// Load a GeoJSON file from a URL
 var geoJsonUrl = 'https://aurashak.github.io/geojson/ne_10m_lakes.json';
 viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
-    stroke: Cesium.Color.HOTPINK,
-    fill: Cesium.Color.PINK.withAlpha(0.5),
+    stroke: Cesium.Color.HOTPINK, // Line color
+    fill: Cesium.Color.PINK.withAlpha(0.5), // Polygon fill color with some transparency
+    strokeWidth: 3
+}));
+
+
+var geoJsonUrl = 'https://aurashak.github.io/geojson/worldcountries.geojson';
+viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
+    stroke: Cesium.Color.HOTPINK, // Line color
+    fill: Cesium.Color.PINK.withAlpha(0.5), // Polygon fill color with some transparency
     strokeWidth: 3
 }));
 
