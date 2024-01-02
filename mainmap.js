8aef49b8c7330190b8392e5be087a352483d4d43
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-01 20:00:02 -0500

    Delete mainmap.js

--- mainmap.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXVyYXNoayIsImEiOiJjbHBwd2dvZXYxNGQ0MnFwanZqeXV6OHV0In0.1ypymm-PTaV5y0Igl5fKzQ';
-
-var map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/aurashk/clqoqcgqx00iv01p5ddg1fknw', // your custom Mapbox style
-    center: [-74.0060, 40.7128], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
