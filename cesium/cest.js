aca8d8e99fa12fb97bb1c98c4223799a81d4e346
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-02 19:13:53 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -80,12 +80,7 @@ viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
 }));
 
 
-var geoJsonUrl = 'https://aurashak.github.io/geojson/worldcountries.geojson';
-viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
-    stroke: Cesium.Color.BLACK, // Line color
-    fill: new Cesium.Color(1, 1, 1, 0), // Fully transparent fill color
-    strokeWidth: 1
-}));
+
 
 
 
