cbdcf53c2f558c994fca009d162f1019fececc12
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-02 21:41:39 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -87,7 +87,12 @@ viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
     strokeWidth: 1
 }));
 
-
+var geoJsonUrl = 'https://aurashak.github.io/geojson/europe.json';
+viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
+    stroke: Cesium.Color.YELLOW, // Line color
+    fill: new Cesium.Color(1, 1, 1, 0.5), // Fully transparent fill color
+    strokeWidth: 1
+}));
 
 
 var geoJsonUrl = 'https://aurashak.github.io/geojson/rivers.geojson';
