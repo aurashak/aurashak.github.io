62897f5c4678abf3172cb9158a684240dc087ec5
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-02 21:21:10 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -75,7 +75,7 @@ viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
 
 var geoJsonUrl = 'https://aurashak.github.io/geojson/satwo.geojson';
 viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
-    stroke: Cesium.Color.BLACK, // Line color
+    stroke: Cesium.Color.YELLOW, // Line color
     fill: new Cesium.Color(1, 1, 1, 0.5), // Fully transparent fill color
     strokeWidth: 1
 }));
