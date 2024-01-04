8e3cdf4673eb7fcf13197fca6edbe6385ff7153b
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-03 22:58:43 -0500

    Add files via upload

--- cesium/cest.js
+++ cesium/cest.js
@@ -1,9 +1,55 @@
-// script.js file
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
+
+var imageryViewModels = [
+    // ESRI World Imagery layer
+    new Cesium.ProviderViewModel({
+        name: 'ESRI World Imagery',
+        iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/esriWorldImagery.png'),
+        tooltip: 'ESRI World Imagery provided by ArcGIS Online',
+        creationFunction: function() {
+            return new Cesium.ArcGisMapServerImageryProvider({
+                url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+            });
+        }
+    }),
+    // OpenStreetMap layer
+    new Cesium.ProviderViewModel({
+        name: 'OpenStreetMap',
+        iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/openStreetMap.png'),
+        tooltip: 'OpenStreetMap (OSM)',
+        creationFunction: function() {
+            return new Cesium.OpenStreetMapImageryProvider({
+                url: 'https://a.tile.openstreetmap.org/'
+            });
+        }
+    }),
+    // Natural Earth II layer
+    new Cesium.ProviderViewModel({
+        name: 'Natural Earth II',
+        iconUrl: Cesium.buildModuleUrl('Widgets/Images/ImageryProviders/naturalEarthII.png'),
+        tooltip: 'Natural Earth II, light-colored base map with fine detail',
+        creationFunction: function() {
+            return new Cesium.TileMapServiceImageryProvider({
+                url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+            });
+        }
+    })
+];
+
+var terrainViewModels = [
+    // Cesium World Terrain layer
+    new Cesium.ProviderViewModel({
+        name: 'Cesium World Terrain',
+        iconUrl: Cesium.buildModuleUrl('Widgets/Images/TerrainProviders/CesiumWorldTerrain.png'),
+        tooltip: 'Cesium World Terrain, high-resolution global terrain model',
+        creationFunction: function() {
+            return Cesium.createWorldTerrain();
+        }
+    })
+];
 
 // Create the viewer with the desired options
 var viewer = new Cesium.Viewer('cesiumContainer', {
-    // ... other options ...
     skyBox: false,
     skyAtmosphere: false,
     timeline: false,
@@ -15,10 +61,12 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
     baseLayerPicker: true,
     animation: false,
     vrButton: false,
-    infoBox: true
+    infoBox: true,
+    imageryProvider: false, // Disable the default imagery layer
+    terrainProvider: false, // Disable the default terrain layer
+    imageryProviderViewModels: imageryViewModels,
+    terrainProviderViewModels: terrainViewModels
 });
-
-
 
 // Load a GeoJSON file from a URL
 var geoJsonUrl = 'https://aurashak.github.io/geojson/projectmarkers.geojson';
@@ -27,7 +75,6 @@ viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geoJsonUrl, {
     fill: Cesium.Color.fromCssColorString('#3b3b3b').withAlpha(1),    
     strokeWidth: .25
 }));
-
 
 
 // Add a handler for mouse move events to display feature name and lat/long
