413ca4f811b3f0e530e89e424b61cc7e1ccd5958
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-05 22:48:38 -0500

    Add files via upload

--- test/testm.js
+++ test/testm.js
@@ -195,19 +195,31 @@ function onEachFeature(feature, layer) {
     layer.on({
         mouseover: function(e) {
             var layer = e.target;
-            var tooltipContent = '';
-            
+
+            // Update hover info content
+            var hoverInfo = document.getElementById('hover-info');
+            var content = '';
+
+            // Check if the feature has a name property and add it to the content
             if (feature.properties && feature.properties.name) {
-                tooltipContent += 'Name: ' + feature.properties.name + '<br>';
+                content += 'Name: ' + feature.properties.name + '<br>';
             }
 
-            var latlng = layer.getBounds ? layer.getBounds().getCenter() : e.latlng;
-            tooltipContent += 'Lat: ' + latlng.lat.toFixed(5) + '<br>Lng: ' + latlng.lng.toFixed(5);
+            // Add latitude and longitude
+            if (layer.getLatLng) {
+                var latlng = layer.getLatLng();
+                content += 'Lat: ' + latlng.lat.toFixed(5) + '<br>Lng: ' + latlng.lng.toFixed(5);
+            } else if (layer.getBounds) {
+                var center = layer.getBounds().getCenter();
+                content += 'Lat: ' + center.lat.toFixed(5) + '<br>Lng: ' + center.lng.toFixed(5);
+            }
 
-            document.getElementById('info').innerHTML = tooltipContent;
+            // Set the content
+            hoverInfo.innerHTML = content;
         },
         mouseout: function(e) {
-            document.getElementById('info').innerHTML = 'Hover over a feature';
+            // Clear hover info when not hovering over a feature
+            document.getElementById('hover-info').innerHTML = 'Hover over a feature';
         }
     });
 }
@@ -293,19 +305,3 @@ function onEachFeature(feature, layer) {
         });
     }
 });
-
-
-// Function to display a tooltip on map click
-function onMapClick(e) {
-    var latlng = e.latlng;
-    var tooltipContent = "Latitude: " + latlng.lat.toFixed(5) + "<br>Longitude: " + latlng.lng.toFixed(5);
-    
-    // You can use a popup or a tooltip to show the coordinates
-    L.popup()
-        .setLatLng(latlng)
-        .setContent(tooltipContent)
-        .openOn(mymap);
-}
-
-// Add the click event listener to your map
-mymap.on('click', onMapClick);
