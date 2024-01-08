c007c54d6c023ceb24770d83aa91800b7f51348c
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-08 14:33:05 -0500

    Add files via upload

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -252,14 +252,14 @@ function removeAllLayersExceptProjectMarkers() {
     }
 
 
-    function createPulsatingIcon() {
-        return L.divIcon({
-            className: 'pulsating-marker red', // Use your CSS classes for red pulsating marker
-            iconSize: [10, 10], // Size of the icon
-            iconAnchor: [5, 5] // Anchor point of the icon
-        });
-    }
-    
+function createPulsatingIcon() {
+    return L.divIcon({
+        className: 'pulsating-marker red', // Use your CSS classes for red pulsating marker
+        iconSize: [10, 10], // Size of the icon
+        iconAnchor: [8, 27.33], // 2/3 of the original size
+    });
+}
+
 
 // Marker Icons
     var redIcon = L.icon({ 
