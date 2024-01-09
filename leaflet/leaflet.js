c9e005e2f61480126ead27f07192c448ce537e4a
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-08 20:20:25 -0500

    Add files via upload

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -6,45 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
     }).addTo(mymap);
 
     function selectIcon(feature) {
-        var className = 'pulsating-marker'; // Base class for all markers
-
-        switch (feature.properties['marker-color']) {
-            case '#FF0000':
-                className += ' red';
-                break;
-            case '#00FF00':
-                className += ' green';
-                break;
-            case '#7F00FF':
-                className += ' violet';
-                break;
-            case '#FFFF00':
-                className += ' yellow';
-                break;
-            default:
-                className += ''; // No additional class for default
-        }
-
+        // Use a single class for all markers
         return L.divIcon({
-            className: className,
+            className: 'basic-marker',
             html: '<div></div>',
-            iconSize: L.point(10, 10),  // Adjust if necessary
-            iconAnchor: [10, 10]        // Adjust if necessary
+            iconSize: L.point(20, 20),  // Size of the icon
+            iconAnchor: [10, 10]        // Position of the icon
         });
-        
     }
 
     function addProjectMarkers() {
         fetch('https://aurashak.github.io/geojson/projectmarkers.geojson')
             .then(response => response.json())
             .then(data => {
-                console.log('GeoJSON data:', data); // Log to check the data
                 L.geoJSON(data, {
-                    onEachFeature: function(feature, layer) {
-                        // Additional interactions can be added here
-                    },
                     pointToLayer: function(feature, latlng) {
-                        console.log('Creating marker for:', feature.properties.name); // Log to check the feature
                         return L.marker(latlng, { icon: selectIcon(feature) });
                     }
                 }).addTo(mymap);
