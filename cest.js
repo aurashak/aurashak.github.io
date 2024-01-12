7ec54c9a0a2b539af3a40ecb3bd4cf41823c5a70
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-12 14:30:59 -0500

    Add files via upload

--- cest.js
+++ cest.js
@@ -1,4 +1,4 @@
-
+`
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
 var viewer = new Cesium.Viewer('cesiumContainer', {
@@ -252,3 +252,4 @@ window.onload = function() {
     });
 
 
+`
