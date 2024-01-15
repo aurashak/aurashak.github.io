322762e970fde49f88fe0b0bc72c25088028c735
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-15 15:08:13 -0500

    Add files via upload

--- homeglobe.js
+++ homeglobe.js
@@ -7,23 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
         terrainProvider: Cesium.createWorldTerrain(),
     });
 
-    // Add a simple entity (marker) on the globe
-    viewer.entities.add({
-        name: 'Sample Location',
-        position: Cesium.Cartesian3.fromDegrees(-74.0707383, 40.7117244, 0),
-        point: {
-            pixelSize: 10,
-            color: Cesium.Color.RED,
-        },
-    });
+ // Add Sentinel-2 imagery layer
+ var sentinel2ImageryProvider = new Cesium.IonImageryProvider({ assetId: 3954 });
 
-    // Set the camera view to a specific location and zoom level
-    viewer.camera.setView({
-        destination: Cesium.Cartesian3.fromDegrees(-74.0707383, 40.7117244, 1000000),
-        orientation: {
-            heading: Cesium.Math.toRadians(0.0),
-            pitch: Cesium.Math.toRadians(-45.0),
-            roll: 0.0,
-        },
-    });
-});
+ viewer.imageryLayers.addImageryProvider(sentinel2ImageryProvider);
+
+ // Add a simple entity (marker) on the globe
+ viewer.entities.add({
+     name: 'Sample Location',
+     position: Cesium.Cartesian3.fromDegrees(-74.0707383, 40.7117244, 0),
+     point: {
+         pixelSize: 10,
+         color: Cesium.Color.RED,
+     },
+ });
