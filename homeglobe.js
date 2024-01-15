0997c7aeb05d515431db1a134eb2f3f7f89f9072
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-15 13:52:48 -0500

    Add files via upload

--- homeglobe.js
+++ homeglobe.js
@@ -1,13 +1,7 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     // Your Cesium code here
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
-    var viewer = new Cesium.Viewer('cesiumContainer', {
-        terrainProvider: Cesium.createWorldTerrain(),
-    });
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmE0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
-    
-    
     // Initialize Cesium Viewer
     var viewer = new Cesium.Viewer('cesiumContainer', {
         terrainProvider: Cesium.createWorldTerrain(),
@@ -32,4 +26,4 @@ document.addEventListener("DOMContentLoaded", function () {
             roll: 0.0,
         },
     });
-
+});
