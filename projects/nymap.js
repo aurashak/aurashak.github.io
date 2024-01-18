c877b24529a222c6957eadd9b7de96c2f8287f28
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-18 11:25:31 -0500

    Add files via upload

--- projects/nymap.js
+++ projects/nymap.js
@@ -1,18 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Define the bounds of the New York City metropolitan region
-    var southWest = L.latLng(40.377399, -74.259090),
-        northEast = L.latLng(41.134986, -73.700180),
-        bounds = L.latLngBounds(southWest, northEast);
-
-    // Initialize the map with a specific location and zoom level
-    var map = L.map('map', {
-        center: [40.7128, -74.0060], // New York City coordinates
-        zoom: 10, // Initial zoom level
-        minZoom: 10, // Minimum zoom level to restrict zooming out
-        maxBounds: bounds, // Restrict panning to the New York City metropolitan region
-        maxBoundsViscosity: 1.0 // Make the map bounce back when dragged outside the bounds
-    });
-
+var map = L.map('map').setView([40.7128, -74.0060], 10);
 
 
 // Load and add the 100 year floodplain GeoJSON layer
