bd77416edce515598940310920528d3f94dc8e8e
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-21 12:46:46 -0500

    Add files via upload

--- homeglobe.js
+++ homeglobe.js
@@ -1,7 +1,7 @@
 
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
-var viewer = new Cesium.Viewer('cesiumContainer', {
+var viewer = new Cesium.Viewer('cesiumContainer1', {
     baseLayerPicker: false,
     imageryProvider: false,
 
