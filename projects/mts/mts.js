3b72f155e95f9dbd084c60f87c9b24b000bda7e2
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-22 00:57:16 -0500

    Add files via upload

--- projects/mts/mts.js
+++ projects/mts/mts.js
@@ -2,32 +2,43 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXVyYXNoayIsImEiOiJjbHBwd2dvZXYxNGQ0MnFwanZqe
 
 const map = new mapboxgl.Map({
     container: 'mtsmap',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: {
+        version: 8,
+        sources: {
+            'streets': {
+                type: 'vector',
+                url: 'mapbox://mapbox.mapbox-streets-v8',
+            },
+            'buildings': {
+                type: 'vector',
+                url: 'mapbox://mapbox.mapbox-streets-v8',
+            },
+        },
+        layers: [
+            {
+                id: 'buildings-layer',
+                type: 'fill-extrusion',
+                source: 'buildings',
+                'source-layer': 'building',
+                filter: ['==', 'extrude', 'true'],
+                minzoom: 15,
+                paint: {
+                    'fill-extrusion-color': '#aaa',
+                    'fill-extrusion-height': ['get', 'height'],
+                    'fill-extrusion-base': ['get', 'min_height'],
+                    'fill-extrusion-opacity': 0.6,
+                },
+            },
+        ],
+    },
     center: [-73.954, 40.812], // Centered around the West Side of Manhattan and the Hudson River
-    zoom: 22, // Zoom in for a close-up view
+    zoom: 18, // Zoom in for a close-up view
     pitch: 45, // Set pitch to 45 for a 3D perspective
     bearing: -260, // Set bearing for a view from the East Hudson River
-
-    maxZoom: 22, // Limit the maximum zoom level
-});
-
-// Add 3D building layer
-map.on('load', () => {
-    map.addLayer({
-        'id': '3d-buildings',
-        'source': 'composite',
-        'source-layer': 'building',
-        'filter': ['==', 'extrude', 'true'],
-        'type': 'fill-extrusion',
-        'minzoom': 15,
-        'paint': {
-            'fill-extrusion-color': '#aaa',
-            'fill-extrusion-height': ['get', 'height'],
-            'fill-extrusion-base': ['get', 'min_height'],
-            'fill-extrusion-opacity': 0.6,
-        },
-    });
+    minZoom: 17,
+    maxZoom: 20, // Limit the maximum zoom level
 });
 
 // Add zoom and rotation controls
 map.addControl(new mapboxgl.NavigationControl());
+
