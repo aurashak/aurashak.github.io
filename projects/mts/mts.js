6837d9ef19dd9485dbb8b1cef5978756827cd15a
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-22 13:56:22 -0500

    Add files via upload

--- projects/mts/mts.js
+++ projects/mts/mts.js
@@ -12,39 +12,55 @@ var map = new mapboxgl.Map({
     center: [-73.957611, 40.822583], // Centered around the West Side of Manhattan and the Hudson River
   });
 
+// Add a variable to track label visibility
+var labelsVisible = true;
+
 // Add 3D building layer
 map.on('load', function () {
-    map.addLayer({
-      'id': '3d-buildings',
-      'source': 'composite',
-      'source-layer': 'building',
-      'filter': ['==', 'extrude', 'true'],
-      'type': 'fill-extrusion',
-      'minzoom': 14, // Adjust minimum zoom level to show 3D buildings
-      'paint': {
-        'fill-extrusion-color': '#aaa',
-        'fill-extrusion-height': [
-          'interpolate',
-          ['linear'],
-          ['zoom'],
-          15,
-          0,
-          15.05,
-          ['get', 'height'],
-        ],
-        'fill-extrusion-base': [
-          'interpolate',
-          ['linear'],
-          ['zoom'],
-          15,
-          0,
-          15.05,
-          ['get', 'min_height'],
-        ],
-        'fill-extrusion-opacity': 0.6,
-      },
-    });
+  map.addLayer({
+    'id': '3d-buildings',
+    'source': 'composite',
+    'source-layer': 'building',
+    'filter': ['==', 'extrude', 'true'],
+    'type': 'fill-extrusion',
+    'minzoom': 14,
+    'paint': {
+      'fill-extrusion-color': '#aaa',
+      'fill-extrusion-height': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        15,
+        0,
+        15.05,
+        ['get', 'height'],
+      ],
+      'fill-extrusion-base': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        15,
+        0,
+        15.05,
+        ['get', 'min_height'],
+      ],
+      'fill-extrusion-opacity': 0.6,
+    },
   });
+});
+
+// Add a click event handler for the toggle button
+document.getElementById('toggleLabelsButton').addEventListener('click', function () {
+  if (labelsVisible) {
+    // Hide labels
+    map.setLayoutProperty('3d-buildings', 'visibility', 'none');
+    labelsVisible = false;
+  } else {
+    // Show labels
+    map.setLayoutProperty('3d-buildings', 'visibility', 'visible');
+    labelsVisible = true;
+  }
+});
 
 // Add zoom and rotation controls
 map.addControl(new mapboxgl.NavigationControl());
