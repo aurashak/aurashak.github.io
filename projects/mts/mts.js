d96e2274978563ac82831727d58ddcc03b09a57f
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-22 00:23:25 -0500

    Add files via upload

--- projects/mts/mts.js
+++ projects/mts/mts.js
@@ -4,9 +4,9 @@ const map = new mapboxgl.Map({
     container: 'mtsmap',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-73.954, 40.812], // Centered around the West Side of Manhattan and the Hudson River
-    zoom: 70, // Zoom in for a close-up view
+    zoom: 90, // Zoom in for a close-up view
     pitch: 45, // Set pitch to 45 for a 3D perspective
-    bearing: -230, // Set bearing for a view from the East Hudson River
+    bearing: -260, // Set bearing for a view from the East Hudson River
     maxBounds: [
         [-74.0479, 40.7074], // Lower Manhattan's southwest corner
         [-73.9667, 40.8876]  // Upper Manhattan's northeast corner
