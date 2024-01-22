9681b415e9be020ea1c95683cb5160b98d7cdb6d
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-21 21:10:40 -0500

    Add files via upload

--- homeglobe.js
+++ homeglobe.js
@@ -16,7 +16,7 @@ var viewer = new Cesium.Viewer('cesiumContainer1', {
     shouldAnimate: true, // You can keep animation true if needed
 });
 
-viewer.scene.backgroundColor = Cesium.Color.fromAlpha(Cesium.Color.BLUE, 0); // 0.5 is the alpha value (range: 0 to 1)
+viewer.scene.backgroundColor = Cesium.Color.WHITE.withAlpha(0);
 
 viewer.camera.setView({
     destination: Cesium.Cartesian3.fromDegrees(-74.0707383, 40.7117244, 15000000),
