7178c6ce7a0912481f7ae903d3c5bf187a6468ce
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-23 12:15:09 -0500

    Add files via upload

--- projects/mts/mts.js
+++ projects/mts/mts.js
@@ -1,17 +1,19 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXVyYXNoayIsImEiOiJjbHBwd2dvZXYxNGQ0MnFwanZqeXV6OHV0In0.1ypymm-PTaV5y0Igl5fKzQ';
 
+
+
+// Create a new Mapbox map instance
 var map = new mapboxgl.Map({
-    container: 'mtsmap',
-    zoom: 16,
-    pitch: 75,
-    bearing: -260,
-    minZoom: 16,
-    maxZoom: 20,
-    style: 'mapbox://styles/aurashk/clrpk291o007z01p5ad9261wx',
-    center: [-73.957611, 40.822583],
+    container: 'map', // HTML element ID where the map will be displayed
+    style: 'mapbox://styles/mapbox/streets-v11', // Map style URL
+    center: [-74.006, 40.7128], // Initial map center coordinates (longitude, latitude)
+    zoom: 12 // Initial zoom level
 });
 
-
-
-// Add zoom and rotation controls
+// Add navigation control (zoom in/out buttons)
 map.addControl(new mapboxgl.NavigationControl());
+
+// Add a marker to the map
+var marker = new mapboxgl.Marker()
+    .setLngLat([-74.006, 40.7128]) // Marker coordinates (longitude, latitude)
+    .addTo(map);
