703f80a97ea47a5eb2154f1af0543286b5b1b429
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-22 23:55:28 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -8,8 +8,8 @@ var bounds = L.latLngBounds(
 var map = L.map('nymap', {
     maxBounds: bounds,          // Set maxBounds to limit zooming out
     maxBoundsViscosity: 1.0,   // Elastic effect on exceeding bounds
-    minZoom: 7,                // Minimum zoom level
-    maxZoom: 10                // Maximum zoom level (adjust as needed)
+    minZoom: 10,                // Minimum zoom level
+    maxZoom: 14                // Maximum zoom level (adjust as needed)
 }).setView([40.7128, -74.0060], 12); // New York City coordinates, closer zoom level
 
 
