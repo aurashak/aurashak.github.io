6eff8e2acf13d9838f76862306a6974ba06d14a9
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-24 15:32:49 -0500

    Add files via upload

--- projects/nmca/nmca.js
+++ projects/nmca/nmca.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOMContentLoaded event fired.');
     // Get the mini and larger illustrations
     var miniIllustration = document.getElementById('miniIllustration');
     var largerIllustration = document.getElementById('largerIllustration');
 
     // Add mouseover event listener to the mini illustration
     miniIllustration.addEventListener('mouseover', function (event) {
-        // Calculate the position of the mouse relative to the mini illustration
-        var mouseX = event.offsetX / miniIllustration.width;
-        var mouseY = event.offsetY / miniIllustration.height;
-
         // Set the corresponding position on the larger illustration
-        largerIllustration.style.objectPosition = mouseX * 100 + '% ' + mouseY * 100 + '%';
+        largerIllustration.style.objectPosition = '100% 100%';
     });
 });
