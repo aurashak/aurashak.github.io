5fa18c38e58327ed3b199aa25d4600326c08deb0
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-24 15:54:04 -0500

    Add files via upload

--- homeglobe.js
+++ homeglobe.js
@@ -1,18 +1,3 @@
-// The viewModel tracks the state of our mini application.
-const viewModel = {
-    brightness: 0,
-    contrast: 0,
-    hue: 0,
-    saturation: 0,
-    gamma: 0,
-};
-// Convert the viewModel members into knockout observables.
-Cesium.knockout.track(viewModel);
-
-// Bind the viewModel to the DOM elements of the UI that call for it.
-const toolbar = document.getElementById("toolbar");
-Cesium.knockout.applyBindings(viewModel, toolbar);
-
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YjAwYzZhZi1hMWY1LTRhYTgtODYwNi05NGEzOWJjYmU0ZWMiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDQxMzQ3OTd9.6JFFAQdUv-HD2IO8V-vcWbk2jn1dsivyu1qrgA1q67c';
 
 var viewer = new Cesium.Viewer('cesiumContainer1', {
@@ -57,14 +42,6 @@ function toggleImageryLayer() {
     isSentinel2Visible = !isSentinel2Visible;
 }
 
-// Get the slider element and its value display element
-var slider = document.getElementById("mySlider");
-var sliderValueDisplay = document.getElementById("sliderValue");
-
-// Update the value display when the slider value changes
-slider.addEventListener("input", function () {
-    sliderValueDisplay.textContent = slider.value;
-});
 
 // Set the initial rotation rate
 var spinRate = 0.0003;
@@ -91,43 +68,3 @@ function updateRotation() {
 // Add a render loop to continuously update the rotation
 viewer.scene.postRender.addEventListener(updateRotation);
 
-// The following code is added for image adjustment
-
-const imageryLayers = viewer.imageryLayers;
-
-// Make the active imagery layer a subscriber of the viewModel.
-function subscribeLayerParameter(name) {
-    Cesium.knockout
-        .getObservable(viewModel, name)
-        .subscribe(function (newValue) {
-            if (imageryLayers.length > 0) {
-                const layer = imageryLayers.get(0);
-                layer[name] = newValue;
-            }
-        });
-}
-
-subscribeLayerParameter("brightness");
-subscribeLayerParameter("contrast");
-subscribeLayerParameter("hue");
-subscribeLayerParameter("saturation");
-subscribeLayerParameter("gamma");
-
-imageryLayers.get(0).saturation = 3.0;
-
-// Make the viewModel react to base layer changes.
-function updateViewModel() {
-    if (imageryLayers.length > 0) {
-        const layer = imageryLayers.get(0);
-        viewModel.brightness = layer.brightness;
-        viewModel.contrast = layer.contrast;
-        viewModel.hue = layer.hue;
-        viewModel.saturation = layer.saturation;
-        viewModel.gamma = layer.gamma;
-    }
-}
-
-imageryLayers.layerAdded.addEventListener(updateViewModel);
-imageryLayers.layerRemoved.addEventListener(updateViewModel);
-imageryLayers.layerMoved.addEventListener(updateViewModel);
-updateViewModel();
