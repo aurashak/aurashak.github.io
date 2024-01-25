e523d325b3c1aef829189f06825ce522893ac17d
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-25 00:24:40 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -1,5 +1,3 @@
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5ZjljMGQ4MS0zNzJkLTQxMjItODk1OC03ZDkwM2VkYzYwMDUiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDYxNTcwNDJ9.9AcXXdl9PUpvx5y-XBR81kIHSjjfRbY6wDM-JPVPCZo';
-
 var viewer = new Cesium.Viewer('mtsmap', {
     terrainProvider: Cesium.createWorldTerrain(),
     baseLayerPicker: false,
@@ -12,31 +10,20 @@ var viewer = new Cesium.Viewer('mtsmap', {
     animation: false,
     timeline: false,
     scene3DOnly: true,
-    sceneMode: Cesium.SceneMode.SCENE3D
+    sceneMode: Cesium.SceneMode.SCENE3D,
+    screenSpaceCameraController: {
+        minimumZoomDistance: 200.0, // Set minimum zoom distance (in meters)
+        maximumZoomDistance: 1000.0 // Set maximum zoom distance (in meters)
+    }
 });
 
 var osm3D = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
-var defaultImageryProvider = viewer.imageryLayers.get(0).imageryProvider
-var satelliteLayer = Cesium.createWorldImagery();
-
-// Set the camera to focus slightly further west and facing east
-viewer.camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(-74.0015, 40.8167, 1500.0), 
-    orientation: {
-        heading: Cesium.Math.toRadians(90),  
-        pitch: Cesium.Math.toRadians(-65),   
-        roll: Cesium.Math.toRadians(0)
-    }
-});
 
 // Function to toggle satellite layer
 function toggleSatellite() {
     var checkbox = document.getElementById('toggleSatellite');
     if (checkbox.checked) {
-        viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
-            url: 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        }));
+        viewer.imageryLayers.addImageryProvider(Cesium.createWorldImagery());
     } else {
         viewer.imageryLayers.remove(viewer.imageryLayers.get(1)); // Assumes satellite layer is at index 1
     }
@@ -44,3 +31,13 @@ function toggleSatellite() {
 
 // Attach event listener to the checkbox
 document.getElementById('toggleSatellite').addEventListener('change', toggleSatellite);
+
+// Set the camera to focus slightly further west, facing east, and at a closer zoom
+viewer.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(-74.0015, 40.8167, 500.0), // Closer zoom (lower altitude)
+    orientation: {
+        heading: Cesium.Math.toRadians(90),  // Rotate to face east
+        pitch: Cesium.Math.toRadians(-25),   // Lower the pitch angle
+        roll: Cesium.Math.toRadians(0)
+    }
+});
