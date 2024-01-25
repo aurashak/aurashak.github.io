a51bd93907f1abf889c12fa6cd4dbdda4d1b7b35
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-24 22:21:59 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -1,7 +1,7 @@
 // Define the bounds for the New York City Tri-State area
 var bounds = L.latLngBounds(
     L.latLng(40.4774, -74.2591), // Southwest corner (bottom-left)
-    L.latLng(41.4754, -73.3913)  // Northeast corner (top-right)
+    L.latLng(41.2, -73.3913)  // Adjusted Northeast corner latitude (top-right)
 );
 
 // Create and configure the map with the specified bounds
