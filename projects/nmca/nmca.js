2854f215215e5c4c976814912983138a41650daf
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-25 00:09:13 -0500

    Add files via upload

--- projects/nmca/nmca.js
+++ projects/nmca/nmca.js
@@ -2,3 +2,8 @@ function zoomTo(x, y) {
     const zoomImage = document.getElementById('zoomImage');
     zoomImage.style.transform = `scale(2) translate(${(x - 0.5) * 100}%, ${(y - 0.5) * 100}%)`;
 }
+
+function resetZoom() {
+    const zoomImage = document.getElementById('zoomImage');
+    zoomImage.style.transform = 'none';
+}
