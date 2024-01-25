784226f2ff3f1ba080fd01eb40c4a3cf799928bd
aurashak <154039021+aurashak@users.noreply.github.com>
2024-01-25 16:54:01 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -2,7 +2,6 @@ Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 
 var viewer = new Cesium.Viewer('mtsmap', {
     terrainProvider: Cesium.createWorldTerrain(),
-
     baseLayerPicker: false,
     geocoder: false,
     homeButton: false,
@@ -13,27 +12,23 @@ var viewer = new Cesium.Viewer('mtsmap', {
     navigationHelpButton: false,
     fullscreenButton: false,
     animation: false,
-    skyBox: false, // Disable the skybox
-    skyAtmosphere: false, // Disable the sky atmosphere
+    skyBox: false,
+    skyAtmosphere: false,
     scene3DOnly: false,
     sceneMode: Cesium.SceneMode.SCENE3D,
 });
 
+var osm3D = viewer.scene.primitives.add(Cesium.createOsmBuildings());
 
-    var osm3D = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-
-
-    // Set the camera to focus slightly further west, facing east, and at a closer zoom
-    viewer.scene.camera.setView({
-        destination: Cesium.Cartesian3.fromDegrees(-73.97421308903137, 40.820382982431454, 500.0), // Adjusted coordinates and altitude
-        orientation: {
-            heading: Cesium.Math.toRadians(90),  
-            pitch: Cesium.Math.toRadians(-25),   
-            roll: Cesium.Math.toRadians(0)
-        }
-    });
-
-
+// Set the camera to focus slightly further west, facing east, and at a closer zoom
+viewer.scene.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(-73.97421308903137, 40.820382982431454, 500.0),
+    orientation: {
+        heading: Cesium.Math.toRadians(90),
+        pitch: Cesium.Math.toRadians(-25),
+        roll: Cesium.Math.toRadians(0),
+    },
+});
 
 // Set bounds around the map
 var westLongitude = -74.0015;
@@ -41,8 +36,13 @@ var eastLongitude = -73.9465;
 var southLatitude = 40.8090;
 var northLatitude = 40.8330;
 
-viewer.camera.viewBoundingRectangle(
-    Cesium.Rectangle.fromDegrees(westLongitude, southLatitude, eastLongitude, northLatitude)
-);
+// Use the postRender event to ensure the scene is ready
+viewer.scene.postRender.addEventListener(function () {
+    viewer.camera.viewBoundingRectangle(
+        Cesium.Rectangle.fromDegrees(westLongitude, southLatitude, eastLongitude, northLatitude)
+    );
+});
 
-
+// Set minimum and maximum zoom distances
+viewer.scene.screenSpaceCameraController.minimumZoomDistance = 200.0;
+viewer.scene.screenSpaceCameraController.maximumZoomDistance = 1000.0;
