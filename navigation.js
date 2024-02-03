4a3a136ad6c5fb6e17e6a8ff4483f7dcdccce5f6
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-02 23:13:56 -0500

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -27,8 +27,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-document.getElementById("projectsLink").addEventListener("click", function() {
-    var projectsList = document.getElementById("projectsList");
-    projectsList.style.display = (projectsList.style.display === "none" || projectsList.style.display === "") ? "block" : "none";
-});
