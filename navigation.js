665f010d2c596fead6867667af1bdcef3d168292
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-02 20:43:26 -0500

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -1,4 +1,28 @@
-document.getElementById("projectsLink").addEventListener("click", function() {
-    var projectsList = document.getElementById("projectsList");
-    projectsList.style.display = (projectsList.style.display === "none" || projectsList.style.display === "") ? "block" : "none";
+document.addEventListener("DOMContentLoaded", function () {
+    // Your navigation code goes here
+    const navigation = document.createElement("nav");
+    navigation.innerHTML = `
+        <div class="navigation"><a href="https://aurashak.github.io">Home</a></div>
+        <div class="navigation"><a href="https://aurashak.github.io/about/about.html">About</a></div>
+        <div class="navigation" id="projectsLink">Projects</div>
+        <div class="hidden-projects" id="projectsList">
+            <a href="https://aurashak.github.io/projects/nymap/nymap.html">NY Map</a>
+            <a href="https://aurashak.github.io/projects/mts/mts.html">MTS</a>
+            <a href="https://aurashak.github.io/projects/nmca/nmca.html">NMCA</a>
+            <a href="https://aurashak.github.io/projects/nydisplacementmap/nydisplacementmap.html">Displacement Map</a>
+        </div>
+        <div class="navigation"><a href="https://aurashak.github.io/mappingresources.html">Mapping Resources</a></div>
+    `;
+    
+    // Append the navigation to the body or a specific element
+    document.body.appendChild(navigation);
+
+
+    document.getElementById("projectsLink").addEventListener("click", function() {
+        var projectsList = document.getElementById("projectsList");
+        projectsList.style.display = (projectsList.style.display === "none" || projectsList.style.display === "") ? "block" : "none";
+
+    });
 });
+
+
