b2c623a8557efccacfc9c837c9ef3ea1558c2eca
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-07 23:12:13 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -22,36 +22,41 @@ Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 
     */
 
-    const viewer = new Cesium.Viewer("mtsmap", {
-        // This is a global 3D Tiles tileset, so disable the globe
-        globe: false,
-        // Disabling the globe means we need to manually re-enable the atmosphere
-        skyAtmosphere: new Cesium.SkyAtmosphere(),
-        // 2D and Columbus View are not currently supported for global 3D Tiles tilesets
+    const viewer = new Cesium.Viewer("cesiumContainer", {
+        timeline: false,
+        animation: false,
         sceneModePicker: false,
-        // Imagery layers are not currently supported for global 3D Tiles tilesets
         baseLayerPicker: false,
-    });
-    
-    (async () => {
-        try {
-            const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(2275207);
-            viewer.scene.primitives.add(tileset);
-        } catch (error) {
-            console.error(error);
-        }
-    
-        // Set the camera to focus slightly further west, facing east, and at a closer zoom
-        viewer.scene.camera.setView({
-            destination: Cesium.Cartesian3.fromDegrees(-73.97421308903137, 40.820382982431454, 500.0),
-            orientation: {
-                heading: Cesium.Math.toRadians(90),
-                pitch: Cesium.Math.toRadians(-25),
-                roll: Cesium.Math.toRadians(0),
-            },
-        });
-    })();
-    
+        // The globe does not need to be displayed,
+        // since the Photorealistic 3D Tiles include terrain
+        globe: false,
+      });
+      
+      // Enable rendering the sky
+      viewer.scene.skyAtmosphere.show = true;
+      
+      // Add Photorealistic 3D Tiles
+      try {
+        const tileset = await Cesium.createGooglePhotorealistic3DTileset();
+        viewer.scene.primitives.add(tileset);
+      } catch (error) {
+        console.log(`Error loading Photorealistic 3D Tiles tileset.
+        ${error}`);
+      }
+      
+      // Point the camera at the Googleplex
+      viewer.scene.camera.setView({
+        destination: new Cesium.Cartesian3(
+          -2693797.551060477,
+          -4297135.517094725,
+          3854700.7470414364
+        ),
+        orientation: new Cesium.HeadingPitchRoll(
+          4.6550106925119925,
+          -0.2863894863138836,
+          1.3561760425773173e-7
+        ),
+      }); 
 
 
 /*
