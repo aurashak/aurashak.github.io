9ddaa00548156aded80356a65d24f21b738d6d92
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-08 18:05:45 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -1,46 +1,36 @@
-// Grant CesiumJS access to your ion assets
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMjAyN2RmMC05ZDQxLTQwM2YtOWZiZC1hMTI5ZDZlMDgyMGIiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDM4MzA3Njh9.5yn30zsnLQltPUj52_wu8sNHKKNeHkGVi267uKmzI3Q";
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZWYwNWEzNi0zMThkLTQ5ZjgtODZmNC01ZWI0ODQ1OWVhYTYiLCJpZCI6MTg2OTM0LCJpYXQiOjE3MDY3MjIxNjN9.JZdCe1eGQfsow46cZGVVG1r8hL1L0E72AzUsFs1Rw8s';
 
-window.onload = function() {
-    const viewer = new Cesium.Viewer("mtsmap", {
-      // ... viewer configuration options
-      globe: false,
-      skyAtmosphere: new Cesium.SkyAtmosphere(),
-      sceneModePicker: false,
-      baseLayerPicker: false,
-    });
-  
-    try {
-      const tileset = await Cesium.IonResource.fromAssetId(2275207).fetch() // Replace with your asset ID
-          .then(resource => Cesium.Cesium3DTileset.fromResource(resource));
-      viewer.scene.primitives.add(tileset);
-    } catch (error) {
-      console.error(`Failed to load tileset: ${error}`);
-    }
-  };
-  
-  
-  
-/*     var viewer = new Cesium.Viewer('mtsmap', {
-        terrainProvider: Cesium.createWorldTerrain(),
-        baseLayerPicker: false,
-        geocoder: false,
-        homeButton: false,
-        infoBox: true,
-        sceneModePicker: false,
-        selectionIndicator: false,
-        timeline: false,
-        navigationHelpButton: false,
-        fullscreenButton: false,
-        animation: false,
-        skyBox: false,
-        skyAtmosphere: false,
-        scene3DOnly: false,
-        sceneMode: Cesium.SceneMode.SCENE3D,
-    });
+var viewer = new Cesium.Viewer('mtsmap', {
+    terrainProvider: Cesium.createWorldTerrain(),
+    baseLayerPicker: false,
+    geocoder: false,
+    homeButton: false,
+    infoBox: true,
+    sceneModePicker: false,
+    selectionIndicator: false,
+    timeline: false,
+    navigationHelpButton: false,
+    fullscreenButton: false,
+    animation: false,
+    skyBox: false,
+    skyAtmosphere: false,
+    scene3DOnly: false,
+    sceneMode: Cesium.SceneMode.SCENE3D,
+});
 
-    */
 
+
+var osm3D = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+// Set the camera to focus slightly further west, facing east, and at a closer zoom
+viewer.scene.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(-73.97421308903137, 40.820382982431454, 500.0),
+    orientation: {
+        heading: Cesium.Math.toRadians(90),
+        pitch: Cesium.Math.toRadians(-25),
+        roll: Cesium.Math.toRadians(0),
+    },
+});
 
 
 
