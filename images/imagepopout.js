17848091375eea065290493a5f3ddb6705d4f918
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-14 00:24:49 -0500

    Add files via upload

--- images/imagepopout.js
+++ images/imagepopout.js
@@ -1,16 +1,19 @@
-const imageContainer = document.querySelector('.image-container');
-const fullScreen = document.getElementById('fullScreen');
-
-imageContainer.addEventListener('click', (event) => {
-  if (event.target.tagName === 'IMG') {
-    const imageUrl = event.target.src;
-    fullScreen.querySelector('img').src = imageUrl;
-    fullScreen.style.visibility = 'visible';
-    fullScreen.style.opacity = '1';
-  }
-});
-
-fullScreen.addEventListener('click', () => {
-  fullScreen.style.visibility = 'hidden';
-  fullScreen.style.opacity = '0';
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const imageContainer = document.querySelector('.image-container');
+    const fullScreen = document.getElementById('fullScreen');
+  
+    imageContainer.addEventListener('click', function (event) {
+      if (event.target.tagName === 'IMG') {
+        const imageUrl = event.target.src;
+        fullScreen.querySelector('img').src = imageUrl;
+        fullScreen.style.visibility = 'visible';
+        fullScreen.style.opacity = '1';
+      }
+    });
+  
+    fullScreen.addEventListener('click', function () {
+      fullScreen.style.visibility = 'hidden';
+      fullScreen.style.opacity = '0';
+    });
+  });
+  
