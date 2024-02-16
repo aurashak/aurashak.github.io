a55e863ac3c2620a7667a2daab855bd91cd9221a
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-16 00:17:18 -0500

    Add files via upload

--- images/imagepopout.js
+++ images/imagepopout.js
@@ -1,19 +1,15 @@
+// imagepopout.js
+
 document.addEventListener('DOMContentLoaded', function () {
-    const imageContainer = document.querySelector('.image-container');
-    const fullScreen = document.getElementById('fullScreen');
-  
-    imageContainer.addEventListener('click', function (event) {
-      if (event.target.tagName === 'IMG') {
-        const imageUrl = event.target.src;
-        fullScreen.querySelector('img').src = imageUrl;
-        fullScreen.style.visibility = 'visible';
-        fullScreen.style.opacity = '1';
-      }
-    });
-  
-    fullScreen.addEventListener('click', function () {
-      fullScreen.style.visibility = 'hidden';
-      fullScreen.style.opacity = '0';
-    });
-  });
-  
+    var fullScreen = document.getElementById('fullScreen');
+    var closeBtn = document.getElementById('closeBtn');
+
+    if (fullScreen && closeBtn) {
+        closeBtn.addEventListener('click', function () {
+            fullScreen.style.visibility = 'hidden';
+            fullScreen.style.opacity = '0';
+        });
+    } else {
+        console.error('Full screen element or close button not found.');
+    }
+});
