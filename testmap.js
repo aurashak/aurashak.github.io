6f41eea4d81f0c2fb032b874a9d733815f352b82
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-26 12:04:43 -0500

    Add files via upload

--- testmap.js
+++ testmap.js
@@ -25,3 +25,20 @@ var geojsonLayer = L.geoJSON.ajax('https://aurashak.github.io/geojson/nyc/remedi
 });
 
 geojsonLayer.addTo(map);
+
+// Console log for debugging
+geojsonLayer.on('data:loaded', function () {
+    console.log('GeoJSON data loaded successfully.');
+});
+
+geojsonLayer.on('data:loading', function () {
+    console.log('Loading GeoJSON data...');
+});
+
+geojsonLayer.on('data:progress', function (e) {
+    console.log('Loading progress:', e.processed, '/', e.total);
+});
+
+geojsonLayer.on('data:error', function (e) {
+    console.error('Error loading GeoJSON data:', e.error);
+});
