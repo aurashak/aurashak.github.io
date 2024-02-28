1f2309fd8f6eedb3c5b96100fcf3e8e25eae2436
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-28 14:43:48 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -1,45 +1,93 @@
-    // Define the bounds for the New York City Tri-State area
-    var bounds = L.latLngBounds(
-        L.latLng(40.4774, -74.2591), // Southwest corner (bottom-left)
-        L.latLng(41.15, -73.3913)  // Adjusted Northeast corner latitude (top-right)
-    );
+// Define the bounds for the New York City Tri-State area
+var bounds = L.latLngBounds(
+    L.latLng(40.4774, -74.2591), // Southwest corner (bottom-left)
+    L.latLng(41.15, -73.3913)  // Adjusted Northeast corner latitude (top-right)
+);
 
-    // Create and configure the map with the specified bounds
-    var map = L.map('nymap', {
-        maxBounds: bounds,
-        maxBoundsViscosity: 1.0,
-        minZoom: 10,
-        maxZoom: 16
-    }).setView([40.7128, -74.0060], 12); // New York City coordinates, closer zoom level
+// Create and configure the map with the specified bounds
+var map = L.map('nymap', {
+    maxBounds: bounds,          // Set maxBounds to limit zooming out
+    maxBoundsViscosity: 1.0,   // Elastic effect on exceeding bounds
+    minZoom: 10,                // Minimum zoom level
+    maxZoom: 16                // Maximum zoom level (adjust as needed)
+}).setView([40.7128, -74.0060], 12); // New York City coordinates, closer zoom level
 
-    L.control.scale().addTo(map);
 
-    // Base Map Layers
-    var satelliteLayer = L.tileLayer('https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2019_3857/default/g/{z}/{y}/{x}.jpg', {
-        style: function (feature) {
-            return {};
-        }
-    });
 
-    var openstreetmapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        style: function (feature) {
-            return {};
-        }
-    }).addTo(map);
+L.control.scale().addTo(map);
 
-    // Load OSMBuildings layer after the base map layers
-    var osmb = new OSMBuildings(map).load('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
+// Function to calculate marker size based on zoom level
+function calculateMarkerSize(zoom) {
+    // Define the updated initial and minimum sizes
+    var initialSize = 16; // Adjust this value to make markers bigger
+    var minSize = 6;
 
-    // Add OSMBuildings layer to base map layers group
-    var baseLayers = {
-        "OpenStreetMap": openstreetmapLayer,
-        "Satellite": satelliteLayer,
-        "OSMBuildings": osmb
-    };
+    // Calculate the size based on zoom level with a minimum size
+    var size = initialSize - (zoom - 3) * 5;
+    return Math.max(size, minSize);
+}
 
-    var layerControl = L.control.layers(baseLayers, null, {
-        position: 'topright'
-    }).addTo(map);
+
+// NYC Counties Layer (Initially hidden)
+var nyccountiesLayer = L.geoJSON.ajax('https://aurashak.github.io/geojson/nyc/nyccounties.geojson', {
+    style: function (feature) {
+        return {
+            fillColor: 'grey',
+            color: 'black',
+            weight: 0.5,
+            opacity: 0.5,
+            fillOpacity: .3
+        };
+    },
+    pointToLayer: function (feature, latlng) {
+        // Get the county name from the 'NAME' property
+        var countyName = feature.properties.NAME;
+
+        // Create a label marker with the county name as the label
+        return L.marker(latlng, {
+            icon: L.divIcon({
+                className: 'leaflet-div-label',
+                html: countyName, // Use the 'NAME' property as the label
+                iconSize: [100, 40] // Adjust the size of the label marker
+            })
+        });
+    }
+});
+
+
+
+
+// Base Map Layers
+var satelliteLayer = L.tileLayer('https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2019_3857/default/g/{z}/{y}/{x}.jpg', {
+    style: function (feature) {
+        return {};
+    }
+});
+
+var openstreetmapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    style: function (feature) {
+        return {};
+    }
+});
+
+openstreetmapLayer.addTo(map);
+
+
+
+var osmb = new OSMBuildings(map).load('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
+
+// Add OSMBuildings layer to base map layers group
+var baseLayers = {
+    "OpenStreetMap": openstreetmapLayer,
+    "Satellite": satelliteLayer,
+    "Outlines": nyccountiesLayer, // Create an empty layer group for "Turn Off"
+    "OSMBuildings": osmb
+};
+
+var layerControl = L.control.layers(baseLayers, null, {
+    position: 'topright' // Position the control in the top right corner
+}).addTo(map);
+
 
 
 
