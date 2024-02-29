84907c032611b8aa4e5343ef5716f80051e9d4ee
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-29 13:08:53 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -93,74 +93,10 @@ var layerControl = L.control.layers(baseLayers, null, {
 
 
 
-    // OSM Building shadows //
-
-    var
-        now,
-        date, time,
-        timeRange, dateRange,
-        timeRangeLabel, dateRangeLabel;
-
-    function changeDate() {
-        var Y = now.getFullYear(),
-            M = now.getMonth(),
-            D = now.getDate(),
-            h = now.getHours(),
-            m = 0;
-
-        timeRangeLabel.innerText = pad(h) + ':' + pad(m);
-        dateRangeLabel.innerText = Y + '-' + pad(M + 1) + '-' + pad(D);
-
-        osmb.date(new Date(Y, M, D, h, m));
-    }
-
-    function onTimeChange() {
-        now.setHours(this.value);
-        now.setMinutes(0);
-        changeDate();
-    }
-
-    function onDateChange() {
-        now.setMonth(0);
-        now.setDate(this.value);
-        changeDate();
-    }
-
-    function pad(v) {
-        return (v < 10 ? '0' : '') + v;
-    }
-
-    timeRange = document.getElementById('time');
-    dateRange = document.getElementById('date');
-    timeRangeLabel = document.querySelector('*[for=time]');
-    dateRangeLabel = document.querySelector('*[for=date]');
-
-    now = new Date;
-    changeDate();
-
-    // init with day of year
-    var Jan1 = new Date(now.getFullYear(), 0, 1);
-    dateRange.value = Math.ceil((now - Jan1) / 86400000);
-
-    timeRange.value = now.getHours();
-
-    timeRange.addEventListener('change', onTimeChange);
-    dateRange.addEventListener('change', onDateChange);
-    timeRange.addEventListener('input', onTimeChange);
-    dateRange.addEventListener('input', onDateChange);
-
-    // ... (rest of the code)
-
-});
-osmb.load('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
 
 
 
-
-
-
-
-// Geojson layers and layer groups //
+// Geojson layers and layer groups
 
 // AQI sites
 var aqisiteLayer = L.geoJSON.ajax('https://aurashak.github.io/geojson/nyc/aqisite.geojson', {
