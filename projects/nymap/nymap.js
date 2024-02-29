e17f0bc3ca25970547f13aad44dfa541b69789a8
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-29 12:27:31 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -69,12 +69,7 @@ var openstreetmapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}
 
 openstreetmapLayer.addTo(map);
 
-// Load OSMBuildings layer after the base map layers
-var osmb = new OSMBuildings(map);
-osmb.on('load', function () {
-    console.log('OSMBuildings layer loaded successfully.');
-});
-osmb.load('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
+
 
 
 var baseLayers = {
@@ -92,107 +87,71 @@ var layerControl = L.control.layers(baseLayers, null, {
 
 
 
-// OSM Buildings layers //
+// Load OSMBuildings layer after the base map layers
+var osmb = new OSMBuildings(map);
+osmb.on('load', function () {
+    console.log('OSMBuildings layer loaded successfully.');
 
-var osmb = new OSMBuildings(map).load();
+    // OSM Building shadows //
 
-var
-  now,
-  date, time,
-  timeRange, dateRange,
-  timeRangeLabel, dateRangeLabel;
+    var
+        now,
+        date, time,
+        timeRange, dateRange,
+        timeRangeLabel, dateRangeLabel;
 
-function changeDate() {
-  var Y = now.getFullYear(),
-    M = now.getMonth(),
-    D = now.getDate(),
-    h = now.getHours(),
-    m = 0;
+    function changeDate() {
+        var Y = now.getFullYear(),
+            M = now.getMonth(),
+            D = now.getDate(),
+            h = now.getHours(),
+            m = 0;
 
-  timeRangeLabel.innerText = pad(h) + ':' + pad(m);
-  dateRangeLabel.innerText = Y + '-' + pad(M+1) + '-' + pad(D);
+        timeRangeLabel.innerText = pad(h) + ':' + pad(m);
+        dateRangeLabel.innerText = Y + '-' + pad(M + 1) + '-' + pad(D);
 
-  osmb.date(new Date(Y, M, D, h, m));
-}
-
-function onTimeChange() {
-  now.setHours(this.value);
-  now.setMinutes(0);
-  changeDate();
-}
-
-function onDateChange() {
-  now.setMonth(0);
-  now.setDate(this.value);
-  changeDate();
-}
-
-function pad(v) {
-  return (v < 10 ? '0' : '') + v;
-}
-
-timeRange = document.getElementById('time');
-dateRange = document.getElementById('date');
-timeRangeLabel = document.querySelector('*[for=time]');
-dateRangeLabel = document.querySelector('*[for=date]');
-
-now = new Date;
-changeDate();
-
-// init with day of year
-var Jan1 = new Date(now.getFullYear(), 0, 1);
-dateRange.value = Math.ceil((now-Jan1)/86400000);
-
-timeRange.value = now.getHours();
-
-timeRange.addEventListener('change', onTimeChange);
-dateRange.addEventListener('change', onDateChange );
-timeRange.addEventListener('input', onTimeChange);
-dateRange.addEventListener('input', onDateChange);
-
-//************************************************************
-
-// load extra information
-function ajax(url, callback) {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState !== 4) {
-      return;
-    }
-    if (!req.status || req.status < 200 || req.status > 299) {
-      return;
+        osmb.date(new Date(Y, M, D, h, m));
     }
 
-    callback(JSON.parse(req.responseText));
-  };
-  req.open('GET', url);
-  req.send(null);
-}
-
-// formatted json output
-function formatJSON(json) {
-  var html = '';
-  for (var key in json) {
-    html += '<em>'+ key +'</em> '+ json[key] +'<br>';
-  }
-  return html;
-}
-
-osmb.click(function(e) {
-  var url = 'https://data.osmbuildings.org/0.2/uejws863/feature/'+ e.feature +'.json';
-  ajax(url, function(json) {
-    var content = '<b>OSM ID '+ e.feature +'</b>';
-    for (var i = 0; i < json.features.length; i++) {
-      content += '<br><em>OSM Part ID</em> '+ json.features[i].id;
-      content += '<br>'+ formatJSON(json.features[i].properties.tags);
+    function onTimeChange() {
+        now.setHours(this.value);
+        now.setMinutes(0);
+        changeDate();
     }
 
-    L.popup({ maxHeight:200, autoPanPaddingTopLeft:[50,50] })
-      .setLatLng(L.latLng(e.lat, e.lon))
-      .setContent(content)
-      .openOn(map);
-  });
+    function onDateChange() {
+        now.setMonth(0);
+        now.setDate(this.value);
+        changeDate();
+    }
+
+    function pad(v) {
+        return (v < 10 ? '0' : '') + v;
+    }
+
+    timeRange = document.getElementById('time');
+    dateRange = document.getElementById('date');
+    timeRangeLabel = document.querySelector('*[for=time]');
+    dateRangeLabel = document.querySelector('*[for=date]');
+
+    now = new Date;
+    changeDate();
+
+    // init with day of year
+    var Jan1 = new Date(now.getFullYear(), 0, 1);
+    dateRange.value = Math.ceil((now - Jan1) / 86400000);
+
+    timeRange.value = now.getHours();
+
+    timeRange.addEventListener('change', onTimeChange);
+    dateRange.addEventListener('change', onDateChange);
+    timeRange.addEventListener('input', onTimeChange);
+    dateRange.addEventListener('input', onDateChange);
+
+    // ... (rest of the code)
+
 });
+osmb.load('https://{s}.data.osmbuildings.org/0.2/anonymous/tile/{z}/{x}/{y}.json');
 
 
 
