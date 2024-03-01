d907abf3b59e32929557e436085aaf1ff8a4100c
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-29 22:28:15 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -40,7 +40,7 @@ const westHarlemBoundingBox = {
 // Disable all input handling to prevent camera movement
 console.log("Disabling input handling to prevent camera movement...");
 viewer.scene.screenSpaceCameraController.enableTranslate = false;
-viewer.scene.screenSpaceCameraController.enableRotate = false;
+viewer.scene.screenSpaceCameraController.enableRotate = true;
 viewer.scene.screenSpaceCameraController.enableZoom = true;
 viewer.scene.screenSpaceCameraController.enableTilt = true;
 viewer.scene.screenSpaceCameraController.enableLook = false;
