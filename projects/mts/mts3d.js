65ef4aaf18bd65f3603fb63fb0400be18afd67fe
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-02 19:45:53 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -119,20 +119,14 @@ const osmBuildingsTileset = viewer.scene.primitives.add(
 const mtscsoResource = await Cesium.IonResource.fromAssetId(2460335);
 const mtscsoDataSource = await Cesium.GeoJsonDataSource.load(mtscsoResource);
 
-// Remove custom styling and height adjustment
+// Modify the billboard color and style before adding the data source
 mtscsoDataSource.entities.values.forEach((entity) => {
   if (entity.billboard) {
-    // Remove custom styling by not changing the billboard color and image
+    // Change the billboard color to red
+    entity.billboard.color = Cesium.Color.RED;
 
-    // Remove height adjustment - you can remove these lines:
-    // if (entity.position) {
-    //   const newPosition = new Cesium.Cartesian3.fromDegrees(
-    //     Cesium.Cartographic.fromCartesian(entity.position.getValue(viewer.clock.currentTime)).longitude,
-    //     Cesium.Cartographic.fromCartesian(entity.position.getValue(viewer.clock.currentTime)).latitude,
-    //     -50
-    //   );
-    //   entity.position.setValue(newPosition);
-    // }
+    // Change the billboard style to Circle using the createCircleImage function
+    entity.billboard.image = createCircleImage();
   }
 });
 
@@ -148,9 +142,23 @@ mtscsoSwitch.addEventListener("change", (event) => {
   }
 });
 
-// Initial load of mtscso with the default styling
+// Initial load of mtscso with the red circle markers
 viewer.dataSources.add(mtscsoDataSource);
 console.log("Initial load of mtscsoDataSource");
+
+// Function to create a red circle image
+function createCircleImage() {
+  const canvas = document.createElement("canvas");
+  canvas.width = 20;
+  canvas.height = 20;
+  const context = canvas.getContext("2d");
+  context.beginPath();
+  context.arc(10, 10, 8, 0, 2 * Math.PI);
+  context.fillStyle = "red";
+  context.fill();
+  return canvas;
+}
+
 
 
 
