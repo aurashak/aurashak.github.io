6e8fc548b5bfce9f9e2cd6bb8479f59a44eb285e
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-04 02:29:40 -0500

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
         <div class="navigation" id="projectsLink">Projects</div>
         <div class="hidden-projects" id="projectsList">
             <a href="https://aurashak.github.io/projects/nymap/nymap.html">NY Map</a>
+            <a href="https://aurashak.github.io/projects/mts/mts.html">WH 3d Model</a>
+
             <a href="https://aurashak.github.io/projects/nmca/nmca.html">NMCA</a>
             <a href="https://aurashak.github.io/projects/nydisplacementmap/nydisplacementmap.html">Displacement Map</a>
             <a href="https://aurashak.github.io/misc/misc.html">Misc</a>
@@ -35,6 +37,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Highlight the "Projects" button when a project page is being viewed
     const projectPages = [
         "https://aurashak.github.io/projects/nymap/nymap.html",
+        "https://aurashak.github.io/projects/mts/mts.html",
+
         "https://aurashak.github.io/projects/nmca/nmca.html",
         "https://aurashak.github.io/projects/nydisplacementmap/nydisplacementmap.html",
         "https://aurashak.github.io/misc/misc.html"
