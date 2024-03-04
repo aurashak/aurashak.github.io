db0c7f03704f2d579ad25dfd4242186350868ce5
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-04 01:23:14 -0500

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -36,9 +36,7 @@ const initializeCesium = async () => {
       },
     });
 
-    // Add a click event listener to the flyback button
-const flybackButton = document.getElementById('flybackButton');
-flybackButton.addEventListener('click', flyToNewYorkCity);
+
 
     // minimum and maximum zoom limits
     viewer.scene.screenSpaceCameraController.minimumZoomDistance = 100;
