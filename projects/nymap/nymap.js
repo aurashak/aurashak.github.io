fe13368b56ed044d542c9da873805b667b3732bc
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-04 00:36:15 -0500

    Add files via upload

--- projects/nymap/nymap.js
+++ projects/nymap/nymap.js
@@ -1001,7 +1001,7 @@ setLegendSymbol('majoroilstorage', 'black', 'circle');
 setLegendSymbol('inactivesolidwastelandfill', 'grey', 'circle');
 setLegendSymbol('floodplain', '#ADD8E6', 'polygon');
 setLegendSymbol('remediationsites', 'red', 'polygon');
-setLegendSymbol('censusTractCheckbox', 'green', 'polygon');
+setLegendSymbol('censusTractLayer', 'green', 'polygon');
 setLegendSymbol('avgIncome', {'$0 - $30,000': '#fee08b', '$30,000 - $60,000': '#fdae61', '$60,000 - $90,000': '#d73027', '$90,000 - $150,000': '#4575b4', '$150,000 - $250,000': '#313695'}, 'polygon', { layout: 'vertical' });
 
 // Legend for Population Layer (white to dark gray colors)
