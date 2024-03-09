6aca54ff18d92eebd9834b038ebf7ccb7e3ab9fe
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-08 23:30:05 -0500

    Add files via upload

--- testmap.js
+++ testmap.js
@@ -1,110 +1,15 @@
-var map = new L.Map('testmap');
-map.setView([52.51836, 13.40438], 16, false);
+// Define the bounds for the New York City Tri-State area
+var bounds = L.latLngBounds(
+  L.latLng(40.4774, -74.2591), // Southwest corner
+  L.latLng(41.15, -73.3913)  // Northeast corner
+);
 
-new L.TileLayer('https://api.mapbox.com/styles/v1/osmbuildings/cjt9gq35s09051fo7urho3m0f/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoib3NtYnVpbGRpbmdzIiwiYSI6IjNldU0tNDAifQ.c5EU_3V8b87xO24tuWil0w', {
-  attribution: '© Map <a href="https://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  maxNativeZoom: 20
-}).addTo(map);
+// Create and configure the map with the specified bounds
+var map = L.map('nymap', {
+  maxBounds: bounds,
+  maxBoundsViscosity: 1.0,   // Elastic effect on exceeding bounds
+  minZoom: 10,
+  maxZoom: 16
+}).setView([40.7128, -74.0060], 12); // New York City coordinates
 
-var osmb = new OSMBuildings(map).load();
-
-//********************************************************
-
-var
-  now,
-  date, time,
-  timeRange, dateRange,
-  timeRangeLabel, dateRangeLabel;
-
-function changeDate() {
-  var Y = now.getFullYear(),
-    M = now.getMonth(),
-    D = now.getDate(),
-    h = now.getHours(),
-    m = 0;
-
-  timeRangeLabel.innerText = pad(h) + ':' + pad(m);
-  dateRangeLabel.innerText = Y + '-' + pad(M+1) + '-' + pad(D);
-
-  osmb.date(new Date(Y, M, D, h, m));
-}
-
-function onTimeChange() {
-  now.setHours(this.value);
-  now.setMinutes(0);
-  changeDate();
-}
-
-function onDateChange() {
-  now.setMonth(0);
-  now.setDate(this.value);
-  changeDate();
-}
-
-function pad(v) {
-  return (v < 10 ? '0' : '') + v;
-}
-
-timeRange = document.getElementById('time');
-dateRange = document.getElementById('date');
-timeRangeLabel = document.querySelector('*[for=time]');
-dateRangeLabel = document.querySelector('*[for=date]');
-
-now = new Date;
-changeDate();
-
-// init with day of year
-var Jan1 = new Date(now.getFullYear(), 0, 1);
-dateRange.value = Math.ceil((now-Jan1)/86400000);
-
-timeRange.value = now.getHours();
-
-timeRange.addEventListener('change', onTimeChange);
-dateRange.addEventListener('change', onDateChange );
-timeRange.addEventListener('input', onTimeChange);
-dateRange.addEventListener('input', onDateChange);
-
-//************************************************************
-
-// load extra information
-function ajax(url, callback) {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function() {
-    if (req.readyState !== 4) {
-      return;
-    }
-    if (!req.status || req.status < 200 || req.status > 299) {
-      return;
-    }
-
-    callback(JSON.parse(req.responseText));
-  };
-  req.open('GET', url);
-  req.send(null);
-}
-
-// formatted json output
-function formatJSON(json) {
-  var html = '';
-  for (var key in json) {
-    html += '<em>'+ key +'</em> '+ json[key] +'<br>';
-  }
-  return html;
-}
-
-osmb.click(function(e) {
-  var url = 'https://data.osmbuildings.org/0.2/uejws863/feature/'+ e.feature +'.json';
-  ajax(url, function(json) {
-    var content = '<b>OSM ID '+ e.feature +'</b>';
-    for (var i = 0; i < json.features.length; i++) {
-      content += '<br><em>OSM Part ID</em> '+ json.features[i].id;
-      content += '<br>'+ formatJSON(json.features[i].properties.tags);
-    }
-
-    L.popup({ maxHeight:200, autoPanPaddingTopLeft:[50,50] })
-      .setLatLng(L.latLng(e.lat, e.lon))
-      .setContent(content)
-      .openOn(map);
-  });
-});
+L.control.scale().addTo(map);
