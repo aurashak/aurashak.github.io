52ae4dd03c7fe144e3b3f780d15063f740e67857
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-18 13:33:35 -0400

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -20,12 +20,8 @@ const initializeCesium = async () => {
   });
 
 
-// Add the default Cesium navigation controls to the specified div
-viewer.extend(Cesium.viewerCesiumNavigationMixin, {
-    controlDiv: document.getElementById('cesiumNavigationControls')
-});
 
-
+  
   var boundingBox = new Cesium.Rectangle(
     Cesium.Math.toRadians(-74.05), // West
     Cesium.Math.toRadians(40.5),   // South
