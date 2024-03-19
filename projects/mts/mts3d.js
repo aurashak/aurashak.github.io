66bd6388840eb8dec64725598ef2874f9ca7a2bd
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-18 22:43:25 -0400

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -1078,20 +1078,17 @@ toggleEJsites(busDepotPolygon, wasteWaterTreatmentPolygon, gasPipelinePolygon, m
 
 
 
-
-
-        
 function zoomIn() {
   var camera = viewer.camera;
   var distance = camera.positionCartographic.height;
   var factor = 0.5; // Change this value to adjust zoom speed
   var newHeight = distance * factor;
-  
+
   console.log("Zooming in...");
   console.log("Previous camera height:", distance);
   console.log("Zoom factor:", factor);
   console.log("New camera height:", newHeight);
-  
+
   camera.setView({
       destination: Cesium.Cartesian3.fromRadians(camera.positionCartographic.longitude, camera.positionCartographic.latitude, newHeight)
   });
@@ -1102,18 +1099,16 @@ function zoomOut() {
   var distance = camera.positionCartographic.height;
   var factor = 2; // Change this value to adjust zoom speed
   var newHeight = distance * factor;
-  
+
   console.log("Zooming out...");
   console.log("Previous camera height:", distance);
   console.log("Zoom factor:", factor);
   console.log("New camera height:", newHeight);
-  
+
   camera.setView({
       destination: Cesium.Cartesian3.fromRadians(camera.positionCartographic.longitude, camera.positionCartographic.latitude, newHeight)
   });
 }
-
-
 
 
 
