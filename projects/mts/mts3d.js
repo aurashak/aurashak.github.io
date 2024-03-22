b737354504b6d89e07cffd4f8bf684761b94d3c2
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-22 12:09:13 -0400

    Add files via upload

--- projects/mts/mts3d.js
+++ projects/mts/mts3d.js
@@ -21,9 +21,9 @@ const initializeCesium = async () => {
 
 // Define bounding box coordinates
 const boundingBox = new Cesium.Rectangle(
-  Cesium.Math.toRadians(-74.05), // West
-  Cesium.Math.toRadians(40.5),   // South
-  Cesium.Math.toRadians(-73.75), // East
+  Cesium.Math.toRadians(-74.97), // West
+  Cesium.Math.toRadians(40.78),   // South
+  Cesium.Math.toRadians(-73.93), // East
   Cesium.Math.toRadians(40.9)    // North
 );
 
