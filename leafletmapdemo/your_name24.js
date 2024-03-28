3e7a6dc29818ada0e904e4a2372a759b683cfbab
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-28 14:25:53 -0400

    Add files via upload

--- leafletmapdemo/your_name24.js
+++ leafletmapdemo/your_name24.js
@@ -1,13 +1,13 @@
 // Initialize Leaflet map
-var map = L.map('nymap').setView([51.505, -0.09], 13); // Set initial center and zoom level
+var map = L.map('nymap').setView([40.7128, -74.0060], 13); // Set center to New York City and zoom level
 
 // Add a tile layer to the map (for example, OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker to the map
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// Add a marker to the map for New York City
+var marker = L.marker([40.7128, -74.0060]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Hello world!</b><br>This is a Leaflet map.").openPopup();
+marker.bindPopup("<b>Hello New York City!</b><br>This is a Leaflet map.").openPopup();
