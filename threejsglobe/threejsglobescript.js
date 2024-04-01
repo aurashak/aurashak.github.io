e593f37ff23d7727f0a2cadcf137744b3576171c
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-31 21:20:50 -0400

    Add files via upload

---
+++ threejsglobe/threejsglobescript.js
@@ -0,0 +1,10 @@
+const body = '499'; // Earth
+const startDate = '2024-03-31';
+const endDate = '2024-04-01';
+
+fetchData(body, startDate, endDate)
+  .then(data => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = data;
+  })
+  .catch(error => console.error(error));
