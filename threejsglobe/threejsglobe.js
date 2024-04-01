9a79ebfe417ac9a33a8ec9dbd9c25f0f5d674231
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-31 21:09:02 -0400

    Add files via upload

--- threejsglobe/threejsglobe.js
+++ threejsglobe/threejsglobe.js
@@ -1,7 +1,23 @@
-fetch('https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1&COMMAND=%27499%27&MAKE_EPHEM=%27YES%27&EPHEM_TYPE=%27OBSERVER%27&START_TIME=%272024-03-31%27&STOP_TIME=%272024-04-01%27&STEP_SIZE=%27600%27&CAL_FORMAT=%27BOTH%27&CSV_FORMAT=%27YES%27')
-  .then(response => response.text())
-  .then(data => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.innerText = data;
-  })
-  .catch(error => console.error(error));
+function fetchData(body, startDate, endDate) {
+    const params = new URLSearchParams({
+      batch: '1',
+      COMMAND: `'${body}'`,
+      MAKE_EPHEM: 'YES',
+      EPHEM_TYPE: 'OBSERVER',
+      START_TIME: startDate,
+      STOP_TIME: endDate,
+      STEP_SIZE: '3600',
+      CAL_FORMAT: 'BOTH',
+      CSV_FORMAT: 'YES',
+    });
+  
+    const apiUrl = `https://ssd.jpl.nasa.gov/horizons_batch.cgi?${params.toString()}`;
+    const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  
+    const url = `${corsProxyUrl}${apiUrl}`;
+  
+    return fetch(url)
+      .then(response => response.text())
+      .then(data => data.trim().split('\n').slice(28).join('\n'))
+      .catch(error => console.error(error));
+  }
