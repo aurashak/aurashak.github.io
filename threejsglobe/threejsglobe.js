4582ed8f856f0cf5a53327aacd8e20722374fd89
aurashak <154039021+aurashak@users.noreply.github.com>
2024-03-31 21:39:05 -0400

    Add files via upload

--- threejsglobe/threejsglobe.js
+++ threejsglobe/threejsglobe.js
@@ -1,23 +1,22 @@
 function fetchData(body, startDate, endDate) {
     const params = new URLSearchParams({
-      batch: '1',
-      COMMAND: `'${body}'`,
-      MAKE_EPHEM: 'YES',
-      EPHEM_TYPE: 'OBSERVER',
-      START_TIME: startDate,
-      STOP_TIME: endDate,
-      STEP_SIZE: '3600',
-      CAL_FORMAT: 'BOTH',
-      CSV_FORMAT: 'YES',
+        batch: '1',
+        COMMAND: `'${body}'`,
+        MAKE_EPHEM: 'YES',
+        EPHEM_TYPE: 'OBSERVER',
+        START_TIME: startDate,
+        STOP_TIME: endDate,
+        STEP_SIZE: '3600',
+        CAL_FORMAT: 'BOTH',
+        CSV_FORMAT: 'YES',
     });
-  
+
     const apiUrl = `https://ssd.jpl.nasa.gov/horizons_batch.cgi?${params.toString()}`;
     const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  
     const url = `${corsProxyUrl}${apiUrl}`;
-  
+
     return fetch(url)
-      .then(response => response.text())
-      .then(data => data.trim().split('\n').slice(28).join('\n'))
-      .catch(error => console.error(error));
-  }
+        .then(response => response.text())
+        .then(data => data.trim().split('\\n').slice(28).join('\\n'))
+        .catch(error => console.error(error));
+}
--- threejsglobe/threejsglobescript.js
+++ threejsglobe/threejsglobescript.js
@@ -3,8 +3,8 @@ const startDate = '2024-03-31';
 const endDate = '2024-04-01';
 
 fetchData(body, startDate, endDate)
-  .then(data => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = data;
-  })
-  .catch(error => console.error(error));
+    .then(data => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.textContent = data;
+    })
+    .catch(error => console.error(error));
