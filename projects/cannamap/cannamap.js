1fbcd64fff4e370608672a455279e8c853d8c7b4
aurashak <154039021+aurashak@users.noreply.github.com>
2024-04-06 00:02:08 -0400

    Add files via upload

--- projects/cannamap/cannamap.js
+++ projects/cannamap/cannamap.js
@@ -1,3 +1,24 @@
+// Initialize the map
+var map = L.map('cannamap', {
+    // Set initial center and zoom level
+    center: [37.0902, -95.7129], // United States center coordinates
+    zoom: 4,
+    // Disable zooming and scrolling
+    zoomControl: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    dragging: false,
+    boxZoom: false,
+    keyboard: false,
+    touchZoom: false
+});
+
+// Add base map tiles with opacity set
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    opacity: 0.5 // Set opacity to 50%
+}).addTo(map);
+
 // Fetch GeoJSON data and add filtered features to the map as a layer
 fetch('https://aurashak.github.io/projects/cannamap/maps/statesandprovinces.geojson')
     .then(response => response.json())
@@ -5,7 +26,7 @@ fetch('https://aurashak.github.io/projects/cannamap/maps/statesandprovinces.geoj
         console.log('Original GeoJSON data:', data); // Log original GeoJSON data
 
         // Filter GeoJSON features to only include those labeled "United States of America" in the "geounit" category
-        var filteredData = data.features.filter(feature => feature.properties.geoununit === "United States of America");
+        var filteredData = data.features.filter(feature => feature.properties.geonunit === "United States of America");
         console.log('Filtered GeoJSON data:', filteredData); // Log filtered GeoJSON data
 
         // Create a new GeoJSON object with the filtered features
@@ -15,7 +36,7 @@ fetch('https://aurashak.github.io/projects/cannamap/maps/statesandprovinces.geoj
         };
         console.log('Filtered GeoJSON:', filteredGeoJSON); // Log filtered GeoJSON object
 
-        // Add filtered GeoJSON layer to the map with custom styles and labels
+        // Add filtered GeoJSON layer to the map with custom styles
         L.geoJSON(filteredGeoJSON, {
             style: function(feature) {
                 return {
@@ -24,13 +45,6 @@ fetch('https://aurashak.github.io/projects/cannamap/maps/statesandprovinces.geoj
                     color: 'black',          // Border color (change to your desired color)
                     weight: .5               // Border weight
                 };
-            },
-            onEachFeature: function(feature, layer) {
-                // Check if the feature has labels in the "postal" category
-                if (feature.properties.postal) {
-                    // Bind the label as a popup to the layer
-                    layer.bindPopup(feature.properties.postal);
-                }
             }
         }).addTo(map);
     })
