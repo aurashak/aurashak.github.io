17939e8ab9fbee7d23c24de08fbfb5ea72d651ce
aurashak <154039021+aurashak@users.noreply.github.com>
2024-04-08 00:14:40 -0400

    Add files via upload

--- projects/emissionsmap/emissionsmap.js
+++ projects/emissionsmap/emissionsmap.js
@@ -3,7 +3,7 @@ proj4.defs('EPSG:54030', '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum
 
 // Initialize the map
 var map = L.map('emissionsmap', {
-    crs: L.CRS.proj4js('EPSG:54030'), // Use the custom CRS
+    crs: L.Proj.CRS('EPSG:54030'), // Use the custom CRS
     center: [0, 0], // Center coordinates to focus on the world
     zoom: 2, // Zoom level adjusted to show the world
     // Disable zooming and scrolling
@@ -40,84 +40,3 @@ geojsonLayer.addTo(map);
 
 // Zoom the map to the extent of the GeoJSON layer
 map.fitBounds(geojsonLayer.getBounds());
-
-
-
-/*
- 
-// Load GeoJSON file for the world map
-var geojsonLayer = new L.GeoJSON.AJAX("countries.geojson");
-
-// Function to create the choropleth map
-function createChoroplethMap(year) {
-    // Filter emissions data for the given year
-    var yearColumn = year.toString();
-    var yearEmissions = mergedData.map(function (feature) {
-        return {
-            'type': 'Feature',
-            'properties': {
-                'name': feature.properties.name,
-                'emissions': feature.properties[yearColumn]
-            },
-            'geometry': feature.geometry
-        };
-    });
-
-    // Create GeoJSON layer with choropleth
-    var choroplethLayer = L.geoJSON(yearEmissions, {
-        style: function (feature) {
-            return {
-                fillColor: getColor(feature.properties.emissions),
-                weight: 1,
-                opacity: 1,
-                color: 'white',
-                fillOpacity: 0.7
-            };
-        },
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup('<b>' + feature.properties.name + '</b><br>Emissions: ' + feature.properties.emissions);
-        }
-    });
-
-    // Add GeoJSON layer to the map
-    choroplethLayer.addTo(map);
-
-    // Create legend
-    var legend = L.control({position: 'bottomright'});
-    legend.onAdd = function () {
-        var div = L.DomUtil.create('div', 'info legend');
-        var grades = [0, 10, 20, 50, 100, 200, 500, 1000];
-        var labels = [];
-        var from, to;
-
-        for (var i = 0; i < grades.length; i++) {
-            from = grades[i];
-            to = grades[i + 1];
-
-            labels.push(
-                '<i style="background:' + getColor(from + 1) + '"></i> ' +
-                from + (to ? '&ndash;' + to : '+'));
-        }
-
-        div.innerHTML = labels.join('<br>');
-        return div;
-    };
-    legend.addTo(map);
-}
-
-// Example usage: create choropleth map for the year 2022
-createChoroplethMap(2022);
-
-// Slider
-var slider = document.getElementById('slider');
-slider.oninput = function () {
-    map.eachLayer(function (layer) {
-        if (layer instanceof L.GeoJSON) {
-            map.removeLayer(layer);
-        }
-    });
-    createChoroplethMap(parseInt(this.value));
-};
-
-
-*/
